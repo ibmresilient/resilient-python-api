456d11156979919327341eae8f7989470f6b101f
Hugh Pyle <hpyle@resilientsystems.com>
2015-05-31 20:53:04 -0400

    License and name changes in the Python API

--- python/co3/setup.py
+++ python/co3/setup.py
@@ -1,30 +1,30 @@
-# Co3 Systems, Inc. ("Co3") is willing to license software or access to 
-# software to the company or entity that will be using or accessing the 
-# software and documentation and that you represent as an employee or 
-# authorized agent ("you" or "your" only on the condition that you 
-# accept all of the terms of this license agreement.
+# Resilient Systems, Inc. ("Resilient") is willing to license software
+# or access to software to the company or entity that will be using or
+# accessing the software and documentation and that you represent as
+# an employee or authorized agent ("you" or "your") only on the condition
+# that you accept all of the terms of this license agreement.
 #
-# The software and documentation within Co3's Development Kit are 
-# copyrighted by and contain confidential information of Co3. By 
-# accessing and/or using this software and documentation, you agree 
-# that while you may make derivative works of them, you:
+# The software and documentation within Resilient's Development Kit are
+# copyrighted by and contain confidential information of Resilient. By
+# accessing and/or using this software and documentation, you agree that
+# while you may make derivative works of them, you:
 #
-# 1)   will not use the software and documentation or any derivative 
-#      works for anything but your internal business purposes in 
-#      conjunction your licensed used of Co3's software, nor
-# 2)   provide or disclose the software and documentation or any 
-#      derivative works to any third party.
-# 
-# THIS SOFTWARE AND DOCUMENTATION IS PROVIDED "AS IS" AND ANY EXPRESS 
-# OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
-# ARE DISCLAIMED. IN NO EVENT SHALL CO3 BE LIABLE FOR ANY DIRECT, 
-# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
-# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
-# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
-# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+# 1)  will not use the software and documentation or any derivative
+#     works for anything but your internal business purposes in
+#     conjunction your licensed used of Resilient's software, nor
+# 2)  provide or disclose the software and documentation or any
+#     derivative works to any third party.
+#
+# THIS SOFTWARE AND DOCUMENTATION IS PROVIDED "AS IS" AND ANY EXPRESS
+# OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL RESILIENT BE LIABLE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 # OF THE POSSIBILITY OF SUCH DAMAGE.
 
 from __future__ import print_function
@@ -49,17 +49,17 @@ long_description = read('README')
 
 setup(
     name='co3',
-    version='1.0',
-    url='https://www.co3sys.com/',
-    license='Co3 License',
-    author='Co3',
+    version='1.1',
+    url='https://www.resilientsystems.com/',
+    license='Resilient License',
+    author='Resilient',
     install_requires=[
-      'argparse', 
+      'argparse',
       'stomp.py>=4.0.12',
       'requests>=2.3.0'
     ],
-    author_email='support@co3sys.com',
-    description='Co3 API',
+    author_email='support@resilientsystems.com',
+    description='Resilient API',
     long_description=long_description,
     packages=['co3'],
     include_package_data=True,
