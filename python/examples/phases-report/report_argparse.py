bb3b3d8b9314e8eada3770e66848d3565a06644f
Hugh Pyle <hpyle@HPyle-Mpro.local>
2015-07-27 10:29:28 -0400

    Argparse simplify

--- python/examples/phases-report/report_argparse.py
+++ python/examples/phases-report/report_argparse.py
@@ -56,71 +56,11 @@ def valid_date(s):
         msg = "Date must be YYYY-MM-DD: '{0}'.".format(s)
         raise argparse.ArgumentTypeError(msg)
 
-class ReportArgumentParser(argparse.ArgumentParser):
+class ReportArgumentParser(co3.ArgumentParser):
     """Helper to parse command line arguments."""
 
-    DEFAULT_PORT = 443
-
-    def getopt(self, section, opt):
-        """Get a single option value, or None if not present"""
-        if opt in self.config.options(section):
-            return self.config.get(section, opt)
-        return None
-
-    def getopts(self, section, opt):
-        """Get an array of option values, or [] if not present"""
-        if opt in self.config.options(section):
-            return self.config.get(section, opt).split(",")
-        return []
-
     def __init__(self, config_file="report.config"):
-        super(ReportArgumentParser, self).__init__()
-
-        # Read configuration options.
-        self.config = configparser.SafeConfigParser()
-        self.config.read(config_file)
-
-        default_user = self.getopt("resilient", "user")
-        default_password = self.getopt("resilient", "password")
-        default_host = self.getopt("resilient", "host")
-        default_port = self.getopt("resilient", "port") or self.DEFAULT_PORT
-        default_proxy = self.getopts("resilient", "proxy")
-        default_org = self.getopt("resilient", "org")
-        default_cafile = self.getopt("resilient", "cafile")
-
-        self.add_argument("--user",
-                          default=default_user,
-                          required=default_user is None,
-                          help="The email address to use to authenticate to the Resilient server.")
-
-        self.add_argument("--password",
-                          default=default_password,
-                          help="The password to use to authenticate to the Resilient server."
-                               "If omitted, the you will be prompted.")
-
-        self.add_argument("--host",
-                          default=default_host,
-                          required=default_host is None,
-                          help="Resilient server host name")
-
-        self.add_argument("--port",
-                          type=int,
-                          default=default_port,
-                          help="Resilient server REST API port number")
-
-        self.add_argument("--proxy",
-                          default=default_proxy,
-                          nargs="*",
-                          help="An optional HTTP proxy to use when connecting.")
-
-        self.add_argument("--org",
-                          default=default_org,
-                          help="The name of the organization to use.  If you are a member "
-                               "of just one organization, then you can omit this argument.")
-
-        self.add_argument("--cafile",
-                          default=default_cafile,
-                          help="The name of a file that contains trusted certificates.")
+        super(ReportArgumentParser, self).__init__(config_file)
 
         self.add_argument("--since",
                           type=valid_date,
@@ -129,10 +69,5 @@ class ReportArgumentParser(argparse.ArgumentParser):
 
     def parse_args(self, args=None, namespace=None):
         args = super(ReportArgumentParser, self).parse_args(args, namespace)
-
-        password = args.password
-        while not password:
-            password = getpass.getpass("Resilient password for {}: ".format(args.user))
-        args.password = password
 
         return ReportOpts(self.config, vars(args))
