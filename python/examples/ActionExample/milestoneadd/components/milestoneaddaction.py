13fd12b3deb656241a95543f7c19937064f24339
Steven Bade <sbade@resilientsystems.com>
2016-03-25 10:59:39 -0400

    pylint cleanup

--- python/examples/ActionExample/milestoneadd/components/milestoneaddaction.py
+++ python/examples/ActionExample/milestoneadd/components/milestoneaddaction.py
@@ -30,30 +30,19 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 # OF THE POSSIBILITY OF SUCH DAMAGE.
 
+"""
+Action to add a milestone to an incident when the phase changes
+"""
 from __future__ import print_function
+import logging
+import requests
+
+
+from ResilientOrg import ResilientOrg as ResOrg
 from circuits import Component, Debugger
 from circuits.core.handlers import handler
 from resilient_circuits.actions_component import ResilientComponent, ActionMessage
-import os
-import logging
 
-
-import json
-import arrow   # improved Date/Time handling
-import tempfile
-
-from pprint import pprint
-import json
-
-#from lib.ResOrg import ResOrg
-
-#import ResilientOrg as ResOrg
-from ResilientOrg import ResilientOrg as ResOrg
-from pprint import pprint
-from copy import deepcopy
-import time
-
-import requests
 requests.packages.urllib3.disable_warnings()
 
 # Lower the logging threshold for requests
@@ -72,25 +61,21 @@ CONFIG_ACTION_SECTION = 'actiondata'
 
 class MileStoneAddAction(ResilientComponent):
     '''
-
-     invoked when the phase field is changed
-
+    invoked when the phase field is changed
     '''
 
     def __init__(self, opts):
         super(MileStoneAddAction, self).__init__(opts)
         self.options = opts.get(CONFIG_DATA_SECTION, {})
-        self.actiondata = opts.get(CONFIG_ACTION_SECTION,{})
+        self.actiondata = opts.get(CONFIG_ACTION_SECTION, {})
 
         #self.sync_file = os.path.dirname(os.path.abspath(self.sync_opts.get('mapfile')))
 
         # The queue name can be specified in the config file, or default to 'filelookup'
         self.channel = "actions." + self.options.get("queue", "dt_action")
 
-        self.reso = ResOrg(client=self.rest_client)  # set up the resilient connection for the source which 
-                                              # is where the action will get fired from
-                                              # destination will open a unique resorg object each Time
-                                              # a connection needs to be made.
+        # set up for utility functions
+        self.reso = ResOrg(client=self.rest_client)
 
 
     @handler()
@@ -111,9 +96,9 @@ class MileStoneAddAction(ResilientComponent):
         func = self.get_action_function(event.name) # determine which method to invoke based on the event name
 
         if func is not None:
-            rv =func(event)
-            if rv:
-                yield rv
+            retv = func(event)
+            if retv:
+                yield retv
             else:
                 yield "event handled"
         else:
@@ -122,17 +107,13 @@ class MileStoneAddAction(ResilientComponent):
 
         #end _invite_action
 
-    def stubfunction(self,args):
-        # stub function, can be used to test if the action processor has connected properly
-        # create a manual action called "stubfunction" associated with the configured queue
-        # and invoke the manual action.  The log will show that the stub function was invoked
-        log.debug("Stub Function")
-        return "Stub invoked"
-
-    def add_milestone_on_phase_change(self,args):
+    def add_milestone_on_phase_change(self, args):
+        """
+        method that is the action name that gets invoked
+        """
         phase_enum = self.reso.get_phases()
 
-        phase_name = self.reso.map_phase_id(args.message.get('incident').get('phase_id'),phase_enum)
+        phase_name = self.reso.map_phase_id(args.message.get('incident').get('phase_id'), phase_enum)
         if phase_name:
             log.debug("phase name {}".format(phase_name))
             mst = self.reso.CreateMilestone(args.message.get('incident').get('id'),
@@ -141,19 +122,24 @@ class MileStoneAddAction(ResilientComponent):
                                 The phase was changed
                                 '''
                                 )
+            if mst is None:
+                log.error("Failed to create milestone")
+                raise Exception("Milestone failed to be added, check action logs")
+
+
         else:
             log.error("Can not map phase id to name")
 
         return "action complete action completed"
 
 
-    def get_action_function(self,funcname):
+    def get_action_function(self, funcname):
         '''
         map the name passed in to a method within the object
         '''
 
         log.debug("get function {}".format(funcname))
-        return getattr(self,'%s'%funcname,None)
+        return getattr(self, '%s'%funcname, None)
 
 
 
