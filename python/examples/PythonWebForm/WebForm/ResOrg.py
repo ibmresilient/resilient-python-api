7c945254fa7207254cbc6a2a373632f79f3b267c
Steven Bade <sbade@resilientsystems.com>
2016-03-28 15:36:30 -0400

    pylint cleanup of files

--- python/examples/PythonWebForm/WebForm/ResOrg.py
+++ python/examples/PythonWebForm/WebForm/ResOrg.py
@@ -18,39 +18,48 @@
 # THIS SOFTWARE AND DOCUMENTATION IS PROVIDED "AS IS" AND ANY EXPRESS
 # OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL RESILIENT BE LIABLE FOR ANY DIRECT,
+# ARE DISCLAIMED. IN NO EVENT SHALL RESILIENT BE LIABLE FOR ANY DIRECT, 
 # INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
 # (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
 # SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
 # STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 # OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import co3 as resilient
 import os
 import json
+#from collections import OrderedDict
+
+import co3 as resilient
 from WebForm import app
-from collections import OrderedDict
-from pprint import pprint
+
+"""
+resilient access utility module
+"""
 
 
 # Object class for dealing with the resilient organization 
 
 class ResOrg(object):
+    """
+    Utility object
+    """
     def __init__(self):
         self.config = app.config
         self.form_config = None
 
         config = self.config
-        self.client = resilient.SimpleClient(org_name=config['RES_ORG'],
-            base_url="https://{}:{}".format(config['RES_HOST'],config['RES_PORT']),verify=config['RES_CA'])
-        self.client.connect(config['RES_ID'],config['RES_PW'])
+        self.client = resilient.SimpleClient(org_name=config['RES_ORG'], 
+                                             base_url="https://{}:{}".format(config['RES_HOST'],
+                                                                             config['RES_PORT']), 
+                                             verify=config['RES_CA'])
+        self.client.connect(config['RES_ID'], config['RES_PW'])
         self.enums = None
         self.enums = self.get_field_enums()
         self.form_config = self.get_form_config()
         self.create_template = None
-        self.create_template =self.get_template('CreateTemplate')
+        self.create_template = self.get_template('CreateTemplate')
 
 
     # build a dictionary of just the enumerations for fields.
@@ -82,10 +91,13 @@ class ResOrg(object):
 
     # read the configuration for the input form
     def get_form_config(self):
+        """
+        Read the configuration of the form from the file
+        """
         if self.form_config is None:
             aproot = os.path.dirname(os.path.abspath(__file__))
-            formfile = os.path.join(aproot,'config','form_layout.json')
-            with open(formfile,'rb') as ff:
+            formfile = os.path.join(aproot, 'config', 'form_layout.json')
+            with open(formfile, 'rb') as ff:
                 self.form_config = json.load(ff)
 
         return self.form_config
@@ -96,18 +108,24 @@ class ResOrg(object):
 
         return self.enums
 
-    def get_template(self,tempname):
+    def get_template(self, tempname):
+        """
+        load the incident create template
+        """
         if self.create_template is None:
             aproot = os.path.dirname(os.path.abspath(__file__))
-            formfile = os.path.join(aproot,'config',tempname+'.json')
-            with open(formfile,'rb') as ff:
-               template  = json.load(ff)
+            formfile = os.path.join(aproot, 'config', tempname+'.json')
+            with open(formfile, 'rb') as ff:
+               template = json.load(ff)
         return template
 
 
-    def get_config(self,configelement):
+    def get_config(self, configelement):
+        """
+        Get a configuration element from the form layout
+        """
         for i in self.form_config:
-            if i.get('fieldname',None) == configelement:
+            if i.get('fieldname', None) == configelement:
                 return i
         return None
 
