be1db1c74aa7a504e96c1291fa28f6a501b043bd
Katherine Church <kchurch@Katherines-MacBook-Pro.local>
2016-06-16 11:39:24 -0400

    Rollback useless print statements in co3.py

--- python/co3/co3/co3.py
+++ python/co3/co3/co3.py
@@ -123,7 +123,6 @@ class SimpleClient(object):
 
     def _connect(self):
         """Establish a session"""
-        print "VERIFY IN co3._connect is " + str(self.verify)
         response = self.session.post("{0}/rest/session".format(self.base_url),
                                      data=json.dumps(self.authdata),
                                      proxies=self.proxies,
@@ -192,7 +191,6 @@ class SimpleClient(object):
           SimpleHTTPException - if an HTTP exception occurrs.
         """
         url = "{0}/rest/orgs/{1}{2}".format(self.base_url, self.org_id, uri)
-
         response = self._execute_request(self.session.get,
                                          url,
                                          proxies=self.proxies,
