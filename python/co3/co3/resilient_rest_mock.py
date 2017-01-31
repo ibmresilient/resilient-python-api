104d10a4fce308004aab034625a6cf1317d1c80f
Katherine Church <kchurch@Katherines-MacBook-Pro.local>
2017-01-31 11:42:04 -0500

    Move Rest Mock to Co3, Move license info to distributable package

--- python/co3/co3/resilient_rest_mock.py
+++ python/co3/co3/resilient_rest_mock.py
@@ -1,32 +1,3 @@
-# Resilient Systems, Inc. ("Resilient") is willing to license software
-# or access to software to the company or entity that will be using or
-# accessing the software and documentation and that you represent as
-# an employee or authorized agent ("you" or "your") only on the condition
-# that you accept all of the terms of this license agreement.
-#
-# The software and documentation within Resilient's Development Kit are
-# copyrighted by and contain confidential information of Resilient. By
-# accessing and/or using this software and documentation, you agree that
-# while you may make derivative works of them, you:
-#
-# 1)  will not use the software and documentation or any derivative
-#     works for anything but your internal business purposes in
-#     conjunction your licensed used of Resilient's software, nor
-# 2)  provide or disclose the software and documentation or any
-#     derivative works to any third party.
-#
-# THIS SOFTWARE AND DOCUMENTATION IS PROVIDED "AS IS" AND ANY EXPRESS
-# OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL RESILIENT BE LIABLE FOR ANY DIRECT,
-# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
-# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
-# OF THE POSSIBILITY OF SUCH DAMAGE.
-
 """ Requests mock for Resilient REST API """
 
 import logging
@@ -65,10 +36,10 @@ class ResilientMockBase(object):
 class ResilientMock(ResilientMockBase):
     """ Base class for creating Resilient Rest API Mock definitions """
     def __init__(self, org_name=None, email=None):
-        LOG.info("Initialize ResilientMock")
         self.email = email or "api@example.com"
         self.org_name = org_name or "Test Org"
-
+        LOG.info("Initialize ResilientMock %s %s", self.email, self.org_name)
+        
         self.adapter = requests_mock.Adapter()
         for endpoint, handler in self.registered_endpoints.items():
             # Register with regex since some endpoints embed the org_id in the path
