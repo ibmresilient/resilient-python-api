a4d0eb80875bd3f1f7a0d0def8b0e0f934e5f0ab
Katherine Church <kchurch@Katherines-MacBook-Pro.local>
2017-02-23 15:47:31 -0500

    make taskadd example installable, with test

--- python/examples/actions-circuits/taskadd/setup.py
+++ python/examples/actions-circuits/taskadd/setup.py
@@ -1,5 +1,27 @@
 from __future__ import print_function
+
+import sys
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+class PyTest(TestCommand):
+    user_options = [('pytestargs=', 'a', "Resilient Environment Arguments")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytestargs = []
+        self.test_suite = True
+
+    def finalize_options(self):
+        import shlex
+        TestCommand.finalize_options(self)
+        self.test_args = ["-s",] + shlex.split(self.pytestargs)
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
 
 setup(
     name='taskadd',
@@ -10,6 +32,9 @@ setup(
     install_requires=[
         'resilient_circuits>=27.1.0'
     ],
+    tests_require=["pytest",
+                   "pytest_resilient_circuits"],
+    cmdclass = {"test" : PyTest},
     author_email='support@resilientsystems.com',
     description="Resilient Circuits Component for Task Add Menu Item",
     long_description = "Resilient Circuits Component for Task Add Menu Item",
