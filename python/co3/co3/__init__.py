7f91c34d349fde09c50b37016e5b9adfded4b492
Hugh Pyle <hpyle@resilientsystems.com>
2017-04-14 20:42:32 +0100

    Copy finfo and gadget to co3 module
    This makes 'finfo' and 'gadget' install as executables with the co3 library.
    In this commit, the originals are left in place in the 'examples' folder too.

--- python/co3/co3/__init__.py
+++ python/co3/co3/__init__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
-__version__ = "27.0.1"
+__version__ = "27.1.0"
 
 from .co3 import SimpleClient, get_client
 from .co3argparse import ArgumentParser
 from .co3sslutil import match_hostname
+
