d534a743eb96fa5c3c4c6fff8a2933f23ed45a39
Hugh Pyle <hpyle@resilientsystems.com>
2017-08-23 07:55:30 -0400

    Deprecation warning

--- python/co3/co3/__init__.py
+++ python/co3/co3/__init__.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
+import warnings
 from pkg_resources import resource_string
 
 __version__ = resource_string("resilient", "version.txt").strip()
 
 from resilient import *
 
+warnings.warn("The 'co3' module is deprecated, use 'resilient' instead", DeprecationWarning)
