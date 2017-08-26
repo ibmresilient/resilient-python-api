0fd04d216810b78b5604c9ed5c3a30744e7f58c0
Hugh Pyle <hpyle@resilientsystems.com>
2017-08-26 08:08:48 -0400

    Update package exports

--- resilient/resilient/__init__.py
+++ resilient/resilient/__init__.py
@@ -4,7 +4,7 @@ from pkg_resources import resource_string
 
 __version__ = resource_string(__name__, "version.txt").strip()
 
-from .co3 import SimpleClient, PatchConflictException, NoChange, get_client, get_config_file, ensure_unicode, get_proxy_dict
+from .co3 import SimpleClient, SimpleHTTPException, PatchConflictException, NoChange, get_client, get_config_file, ensure_unicode, get_proxy_dict
 from .co3argparse import parse_parameters, ArgumentParser
 from .co3sslutil import match_hostname
 from .patch import Patch
