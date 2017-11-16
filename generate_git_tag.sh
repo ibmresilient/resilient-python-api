c9887b6bc0c72d6d07d982284dfd620028b54530
Nacho Sarnaglia <ignacio.sarnaglia@ibm.com>
2017-11-16 12:19:32 -0300

    fixed creation script

--- generate_git_tag.sh
+++ generate_git_tag.sh
@@ -1,7 +1,6 @@
-# This script will crate a git tag based on the latest tag from git plus the build number.
+# This script will create a git tag based on the latest tag from git plus the build number.
 echo Getting Git Tag...
 latestTag=$(git tag --list | tail -n 1)
-BUILD_NUMBER=30
 
 echo creating tag...
 git tag -a $latestTag.$BUILD_NUMBER -m "$latestTag Build $BUILD_NUMBER"
