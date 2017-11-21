b861b157a52c0c37f6ea2acb8f8626fd18b0c204
Nacho Sarnaglia <ignacio.sarnaglia@ibm.com>
2017-11-21 10:13:11 -0300

    removed hardcoded version and takes major and minor from latest tag

--- generate_git_tag.sh
+++ generate_git_tag.sh
@@ -1,5 +1,5 @@
 # This script will create a git tag adding the jenkins build number.
-libVersion=v28.2
+libVersion=$(git tag --list | tail -n 1 | cut -d "." -f 1,2)
 
 echo Creating tag...
 git tag -a $libVersion.$BUILD_NUMBER -m "$libVersion Build $BUILD_NUMBER"
