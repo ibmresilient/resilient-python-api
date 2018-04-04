f63c8986af38727fc5cd6d1133d87b112bc15217
Hugh Pyle <hpyle@us.ibm.com>
2018-04-04 19:50:09 -0400

    Nope

--- resilient-circuits/resilient_circuits/bin/resilient_circuits_cmd.py
+++ resilient-circuits/resilient_circuits/bin/resilient_circuits_cmd.py
@@ -393,7 +393,7 @@ def main():
     elif args.cmd == "service":
         manage_service(unknown_args + args.service_args, args.res_circuits_args)
     elif args.cmd == "codegen":
-        if args.package is None or args.function is None:
+        if args.package is None and args.function is None:
             codegen_parser.print_usage()
         else:
             logging.basicConfig(format='%(message)s', level=logging.INFO)
