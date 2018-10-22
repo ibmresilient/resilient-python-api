753274e467664fbc3896615f71772d2c5c239390
Mark-Scherfling <mark.scherfling@ibm.com>
2018-10-22 15:31:56 -0400

    INT-659 - fixes for customize -l

--- resilient-circuits/resilient_circuits/util/resilient_customize.py
+++ resilient-circuits/resilient_circuits/util/resilient_customize.py
@@ -169,7 +169,7 @@ def do_customize_resilient(client, entry_points, yflag, install_list):
                     raise
             LOG.info(u"Package '%s' done.", dist)
 
-    if install_list is not None:
+    if install_list is not None and len(install_list) > 0:
         LOG.warn("%s not found. Check package name(s)", install_list)
 
     if ep_count == 0:
