7d87509c55738608fb83a2e00294096ac48e0843
Mark-Scherfling <mark.scherfling@ibm.com>
2019-04-19 15:45:44 -0400

    INT-1337 - corrective logic for cafile = None

--- resilient-circuits/resilient_circuits/actions_component.py
+++ resilient-circuits/resilient_circuits/actions_component.py
@@ -465,8 +465,8 @@ class Actions(ResilientComponent):
             return
 
         # Gather the stomp_cafile for if specified or fallback to the resilient host. Used for TLS / SSL
-        cafile = self.opts.get("stomp_cafile", "false") or self.opts.cafile
-        if cafile.strip().lower() == "false":
+        cafile = self.opts.get("stomp_cafile") or self.opts.cafile
+        if cafile is not None and cafile.strip().lower() == "false":
             # Explicitly disable TLS certificate validation, if you need to
             cafile = None
             LOG.warn(("Unverified STOMP TLS certificate (cafile=false)"))
