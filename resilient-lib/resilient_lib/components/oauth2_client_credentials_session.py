81193a82aa3cf6fc597a21d119a06caf36c4d459
Ihor-Husar <ihor.husar@ibm.com>
2019-07-12 14:17:53 -0400

    INT-1631 client_credentials OAuth2 grant support
    OAuth2ClientCredentials is a wrapper around requests.Session and stores a singleton of each api's session separately.

---
+++ resilient-lib/resilient_lib/components/oauth2_client_credentials_session.py
@@ -0,0 +1,183 @@
+import requests
+import logging
+import time
+
+log = logging.getLogger(__name__)
+
+
+class OAuth2ClientCredentialsSession(requests.Session):
+    """
+    Wrapper around requests.Session that receives authentication tokens through
+    'client_credentials' type of OAuth2 grant, and adds tokens to the requests made through the session,
+    as well as attempts to keep track of the token and refresh it when the time comes.
+
+    This session doesn't requests authorization from the user first, the scope should be pre-authorized.
+
+    Usage:
+    >>> api1 = OAuth2ClientCredentialsSession('https://example1.com/{}/oauth/v2/', tenant_id='xxx',\
+                        client_id='xxx', client_secret='xxx')
+    >>> api2 = OAuth2ClientCredentialsSession('https://example2.com/{}/oauth/v2/', tenant_id='xxx',\
+                        client_id='xxx', client_secret='xxx')
+    >>>
+    >>> api1.post('https://example1.com/v4/me/messages', data={}) # use as a regular requests session object
+    >>> api2.get('https://example2.com/v2/me/updates')
+    """
+
+    AUTHORIZATION_ERROR_CODES = [401]
+
+    def __new__(cls, *args, **kwds):
+        """
+        Creates a singleton per authorization url / API.
+        This means that the same class can be used to access multiple OAuth2 APIs, and each API's session
+        would be a separate singleton with their own managed tokens.
+
+        Guido Van Rossum's Singleton, modified to hold objects in a dictionary
+        with key being request URL, so that multiple CredentialSession could be created from the class.
+        https://www.python.org/download/releases/2.2/descrintro/#__new__
+        """
+        url = None
+
+        if kwds.get('url', None) is not None:
+            url = kwds.get('url')
+        elif len(args) > 1:
+            url = args[0]
+        else:
+            raise ValueError("url is not provided")
+
+        if cls.__dict__.get("__it__", None) is None:
+            cls.__it__ = {}
+        dict_it = cls.__dict__.get("__it__", None)
+        if dict_it.get(url, None) is not None:
+            return dict_it[url]
+
+        it = requests.Session.__new__(cls)
+        dict_it[url] = it
+        return it
+
+    def __init__(self, url=None, tenant_id=None, client_id=None, client_secret=None, scope=None):
+        """
+        Get OAuth2 tokens and save them to be used in session requests.
+        :param url: Pythonic template with authorization url where tenant_id gets inserted
+        :param client_id: API key/User Id
+        :param client_secret: secret for API
+        :param scope: optional, list of scopes
+        """
+        # __init__ is run even if __new__ returned existing instance
+        if self.__dict__.get("authorization_url", None) is not None:
+            return
+
+        super(OAuth2ClientCredentialsSession, self).__init__()
+        log.debug("Creating OAuth2 Session.")
+        if url is None or client_id is None or client_secret is None:
+            raise ValueError("Missing fields required for OAuth2 authentication.")
+
+        if tenant_id is None:
+            tenant_id="common"
+            log.info("tenant_id wasn't provided, defaulting to 'common'.")
+
+        self.tenant_id = tenant_id
+        self.authorization_url = url
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.scope = scope
+
+        self.oauth_token_data = None
+        self.expires_in = None
+        self.access_token = None
+        self.token_type = None
+        self.expiration_time = None
+
+        if not self.authenticate(url, tenant_id, client_id, client_secret, scope):
+            raise ValueError("Wrong credentials for OAuth2 authentication with {0}".format(url))
+
+    def authenticate(self, url, tenant_id, client_id, client_secret, scope):
+        """
+        :param url: String - authorization url - end point for authentication
+        :param tenant_id: String - will default to 'common' if not supplied
+        :param client_id: String
+        :param client_secret: String
+        :param scope: list<String> - list of scopes the token should provide access to
+        :return: True/False - was/wasn't able to authenticate.
+        """
+        token_url = self.get_token_url(url, tenant_id)
+
+        log.debug("Requesting token from {0}".format(url))
+        r = self.get_token(token_url, client_id, client_secret, scope)
+
+        if r.status_code != 200:
+            log.error("Couldn't receive oauth token")
+            return False
+
+        self.oauth_token_data = r.json()
+        log.debug("Received oauth token.")
+        self.expires_in = self.oauth_token_data.get("expires_in", None)
+
+        if self.expires_in is not None:
+            # subtract the time it took to make request, so we don't try to use expired token
+            ttl = self.expires_in - round(r.elapsed.total_seconds())
+            self.expiration_time = time.time() + ttl if ttl >= 0 else None
+        else:
+            self.expiration_time = None
+
+        self.access_token = self.oauth_token_data["access_token"]
+        self.token_type = self.oauth_token_data.get("token_type", None)
+
+        return True
+
+    @staticmethod
+    def get_token_url(url, tenant_id):
+        """
+        Puts together token_url from url's template and tenant_id.
+        """
+        return url.format(tenant_id)
+
+    @staticmethod
+    def get_token(token_url, client_id, client_secret, scope=None):
+        """
+        Override this method if there are any specific things that need to be changed.
+        Such as Cloud Foundry asking grant_type to be 'password' and API key to be passed in 'password'.
+        """
+        post_data = {
+            'client_id': client_id,
+            'client_secret': client_secret,
+            'grant_type': 'client_credentials'
+        }
+        if scope is not None:
+            post_data['scope'] = scope
+        return requests.post(token_url, data=post_data)
+
+    def update_token(self):
+        if not self.authenticate(self.authorization_url, self.tenant_id, self.client_id,
+                                 self.client_secret, self.scope):
+            raise ValueError("Can't update the token, did the credentials for {0} change?".format(self.authorization_url))
+        return True
+
+    def request(self, method, url, *args, **kwargs):
+        """Constructs a :class:`Request <Request>`, injects it with tokens, sends it.
+        If the request fails, likely because it was made before token expired, but expired in the process - retry.
+        Returns :class:`Response <Response>` object.
+        """
+
+        if self.expiration_time is not None:
+            if self.expiration_time < time.time():
+                self.update_token()
+
+        headers = kwargs.get("headers", None)
+        if headers is None:
+            headers = {}
+
+        self.add_authorization_header(headers)
+
+        resp = super(OAuth2ClientCredentialsSession, self).request(method, url, *args, headers=headers, **kwargs)
+
+        now = time.time()
+
+        if resp.status_code in self.AUTHORIZATION_ERROR_CODES and self.expiration_time is not None and \
+                now - resp.elapsed.total_seconds() < self.expiration_time < now:
+            self.update_token()
+            resp = super(OAuth2ClientCredentialsSession, self).request(method, url, *args, headers=headers, **kwargs)
+
+        return resp
+
+    def add_authorization_header(self, headers):
+        headers["Authorization"] = "{} {}".format(self.token_type, self.access_token)
---
+++ resilient-lib/tests/test_oauth2_client_credentials_session.py
@@ -0,0 +1,178 @@
+import pytest
+import requests
+import time
+from resilient_lib.components import oauth2_client_credentials_session
+from resilient_lib.components.oauth2_client_credentials_session import OAuth2ClientCredentialsSession
+
+
+def mock_token_request_factory(elapsed_time=None, expires_in=None, status_code=None, token=None):
+    return lambda *args, **kwargs: MockTokenRequest(elapsed_time=elapsed_time, expires_in=expires_in,
+                                                    status_code=status_code, token=token)
+
+
+def raise_error(*args, **kwargs):
+    raise ValueError()
+
+
+def return_values_each_call(it):
+    class StoreAndReturnValue(object):
+        """
+        Create a generator from iterator, and act like a function, returning
+        next value every time class is called.
+        """
+        def __init__(self, it):
+            self.gen = (x for x in it)
+            self.current = 0
+
+        def __call__(self, *args, **kwargs):
+            self.current += 1
+            return next(self.gen)
+
+    return StoreAndReturnValue(it)
+
+
+class MockTokenRequest(object):
+    class ElapsedTime(object):
+        def __init__(self, elapsed_time):
+            self.elapsed_time = elapsed_time
+
+        def total_seconds(self):
+            return self.elapsed_time
+
+    def __init__(self, elapsed_time=None, expires_in=None, status_code=None, token=None):
+        self.status_code = status_code if status_code is not None else 200
+        self.return_json = {
+            "access_token": "token" if token is None else token
+        }
+
+        if elapsed_time is None:
+            elapsed_time = 3
+        self.elapsed = self.ElapsedTime(elapsed_time)
+
+        if expires_in is not None:
+            self.return_json["expires_in"] = expires_in
+
+    def json(self):
+        return self.return_json
+
+
+class TestAuthenticator(object):
+    @pytest.fixture(autouse=True)
+    def clean_up_singletons(self):
+        if OAuth2ClientCredentialsSession.__dict__.get("__it__", None) is not None:
+            del OAuth2ClientCredentialsSession.__it__
+
+    def test_authenticator_is_singleton(self, monkeypatch):
+        monkeypatch.setattr(OAuth2ClientCredentialsSession, 'authenticate', lambda *_: True)
+        assert OAuth2ClientCredentialsSession("", "", "", "") is OAuth2ClientCredentialsSession("", "", "", "")
+        assert id(OAuth2ClientCredentialsSession("", "", "", "")) == id(OAuth2ClientCredentialsSession("", "", "", ""))
+
+    def test_multiple_authentications_are_singletons(self, monkeypatch):
+        monkeypatch.setattr(OAuth2ClientCredentialsSession, 'authenticate', lambda *_: True)
+        api1_1 = OAuth2ClientCredentialsSession("url:example1", "", "", "")
+        api1_2 = OAuth2ClientCredentialsSession("url:example1", "", "", "")
+
+        api2_1 = OAuth2ClientCredentialsSession("url:example2", "", "", "")
+        api2_2 = OAuth2ClientCredentialsSession("url:example2", "", "", "")
+        assert id(api1_1) == id(api1_2)
+        assert id(api2_1) == id(api2_2)
+        assert id(api1_1) != id(api2_1)
+
+    def test_multiple_sessions_multiple_tokens(self, monkeypatch):
+        monkeypatch.setattr(requests, 'post', mock_token_request_factory(token="api1"))
+        api1 = OAuth2ClientCredentialsSession("url:example1", "", "", "")
+        monkeypatch.setattr(requests, 'post', mock_token_request_factory(token="api2"))
+        api2 = OAuth2ClientCredentialsSession("url:example2", "", "", "")
+
+        assert api1.access_token == "api1"
+        assert api2.access_token == "api2"
+
+        monkeypatch.setattr(requests, 'post', mock_token_request_factory(token="api3"))
+
+        api1_1 = OAuth2ClientCredentialsSession("url:example1", "", "", "")
+        assert api1_1.access_token == "api1"
+        assert OAuth2ClientCredentialsSession("url:example2", "", "", "").access_token == "api2"
+
+    def test_singleton_doesnt_break_classes(self, monkeypatch):
+        monkeypatch.setattr(requests, 'post', mock_token_request_factory(token="api1"))
+        api1_1 = OAuth2ClientCredentialsSession("url:example1", "", "", "")
+        api1_2 = OAuth2ClientCredentialsSession("url:example1", "", "", "")
+        assert isinstance(api1_1, OAuth2ClientCredentialsSession)
+        assert isinstance(api1_2, OAuth2ClientCredentialsSession)
+
+    def test_crates_session_with_passing_authentication(self, monkeypatch):
+        monkeypatch.setattr(requests, 'post', mock_token_request_factory())
+        auth = OAuth2ClientCredentialsSession("test", "test", "test", "test")
+        assert getattr(auth, 'access_token', None) is not None
+
+    def test_fails_to_create_session_with_bad_authentication(self, monkeypatch):
+        monkeypatch.setattr(OAuth2ClientCredentialsSession, 'authenticate', lambda *_: False)
+        with pytest.raises(ValueError):
+            OAuth2ClientCredentialsSession("test", "test", "test", "test")
+
+    def test_authenticator_fails_without_required_fields(self, monkeypatch):
+        monkeypatch.setattr(OAuth2ClientCredentialsSession, 'authenticate', lambda *_: True)
+        with pytest.raises(ValueError):
+            OAuth2ClientCredentialsSession(None, None, None, None)
+        with pytest.raises(ValueError):
+            OAuth2ClientCredentialsSession(None, "", "", "")
+        with pytest.raises(ValueError):
+            OAuth2ClientCredentialsSession("", "", None, "")
+        with pytest.raises(ValueError):
+            OAuth2ClientCredentialsSession("", "", "", None)
+
+    def test_tenant_id_defaults_to_common(self, monkeypatch):
+        monkeypatch.setattr(OAuth2ClientCredentialsSession, 'authenticate', lambda *_: True)
+        test = OAuth2ClientCredentialsSession("", None, "", "")
+        assert test.tenant_id == "common"
+
+    def test_scope_gets_passed_when_given(self, monkeypatch):
+        def confirm_scope_data(*args, data=None, **kwargs):
+            assert data is not None
+            assert data['scope'] is not None
+            return mock_token_request_factory()()
+
+        monkeypatch.setattr(requests, 'post', confirm_scope_data)
+        OAuth2ClientCredentialsSession("test", "test", "test", "test", scope=["scope"])
+
+    def test_no_expiration_doesnt_fail(self, monkeypatch):
+        monkeypatch.setattr(requests, 'post', mock_token_request_factory())
+        auth = OAuth2ClientCredentialsSession("test", "test", "test", "test")
+        assert auth.expiration_time is None
+        assert getattr(auth, 'access_token', None) is not None
+
+    def test_expiration_time_is_set(self, monkeypatch):
+        monkeypatch.setattr(requests, 'post', mock_token_request_factory(expires_in=50))
+        auth = OAuth2ClientCredentialsSession("test", "test", "test", "test")
+        assert auth.expiration_time is not None
+
+    def test_updating_token_after_expiration(self, monkeypatch):
+        monkeypatch.setattr(requests, 'post', mock_token_request_factory(expires_in=50))
+        monkeypatch.setattr(OAuth2ClientCredentialsSession, 'update_token', raise_error)
+        auth = OAuth2ClientCredentialsSession("test", "test", "test", "test")
+        auth.expiration_time = time.time() - 1
+
+        with pytest.raises(ValueError):
+            auth.get("http://google.com")
+
+    def test_detect_updating_token_after_request_ends_after_expiration(self, monkeypatch):
+        """
+        Test that when a request is made while the token is not expired, but the request fails,
+        if sending the request was long enough where token might have expired, that the token will get refreshed,
+        and the request will be made again.
+
+        monkeypatch 'post', because authentication uses POST request
+        monkey patch module's import of time, because pytest also uses time, to create the conditions
+        where token expired during the duration of request being made.
+        """
+        monkeypatch.setattr(requests, 'post', mock_token_request_factory(expires_in=10, elapsed_time=0))
+        monkeypatch.setattr(oauth2_client_credentials_session.time, 'time', return_values_each_call([time.time(), time.time(), time.time() + 15]))
+        monkeypatch.setattr(requests.sessions.Session, 'request',
+                            mock_token_request_factory(status_code=401, elapsed_time=20))
+        monkeypatch.setattr(OAuth2ClientCredentialsSession, 'update_token', raise_error)
+
+        auth = OAuth2ClientCredentialsSession("test", "test", "test", "test")
+
+        with pytest.raises(ValueError):
+            auth.get("http://google.com")
+        assert oauth2_client_credentials_session.time.time.current == 3
