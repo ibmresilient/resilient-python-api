83768f696336e1966989b1815a97c8f00e5f1c8b
yongjian feng <yj@debian64Bit.debian64.irsys.com>
2019-07-19 10:57:30 -0400

    QRadar plugin is still packaged with keyring5.4, even though it does not use it

--- resilient/setup.py
+++ resilient/setup.py
@@ -105,7 +105,7 @@ setup(
             'keyring'
         ],
         ':python_version < "3.5"': [
-            'keyring>=18.0.1,<19.0.0'
+            'keyring>=5.4'
         ]
     },
     tests_require=["pytest>=3.0.0, <4.1.0", ],
