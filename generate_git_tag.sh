231bb88b629edcbebfeb6165f5887f26e865948a
Ihor-Husar <ihor.husar@ibm.com>
2020-01-17 14:55:48 -0500

    Figuring out the build version

--- generate_git_tag.sh
+++ generate_git_tag.sh
@@ -2,15 +2,15 @@
 # If the latest tag 
 latestTag=$(git tag --list | tail -n 1)
 libVersion=$(echo $latestTag | cut -d "." -f 1,2)
-echo "Latest tag $latestTag"
+echo "Currently latest tag $latestTag"
 
 if [[ $latestTag =~ "dev" ]]
 then
+	echo "Creating dev tag $libVersion.$BUILD_NUMBER.dev"
 	git tag -a $libVersion.$BUILD_NUMBER.dev -m "$libVersion dev Build $BUILD_NUMBER"
-	echo "Creating tag... $libVersion.$BUILD_NUMBER.dev"
 else
-	git tag -a $libVersion.$BUILD_NUMBER -m "$libVersion Build $BUILD_NUMBER"
 	echo "Creating tag $libVersion.$BUILD_NUMBER"
+	git tag -a $libVersion.$BUILD_NUMBER -m "$libVersion Build $BUILD_NUMBER"
 fi
 
 
