a3dc7e2482ad1ed0950608e2fbb8f147a0c96ea4
Ihor-Husar <ihor.husar@ibm.com>
2020-01-17 15:19:52 -0500

    Manually set the version for test release
    The need to do it, is because if we change the name of the packages
    to push it to test.pypi it becommes uncommited change which means that
    git describe will create a different descriptor about it, even if we
    create a tag,  because those changes are uncommited
    
    Fixing script for Jenkin's system

--- test_names_update.sh
+++ test_names_update.sh
@@ -1,4 +1,22 @@
+latestTag=$(git tag --list | tail -n 1)
+libVersion=$(echo $latestTag | cut -d "." -f 1,2)
+
+if [[ $latestTag =~ "dev" ]]
+then
+	nextVersion=$(echo "$libVersion.$BUILD_NUMBER.dev")
+else
+	nextVersion=$(echo "$libVersion.$BUILD_NUMBER.dev")
+fi
+
+
 sed -i "s/name='resilient',/name='test_resilient',/g" resilient/setup.py
+sed -i "s/use_scm_version={\"root\": \"\.\.\/\", \"relative_to\": __file__},/version=\"$nextVersion\",/g" resilient/setup.py
+
 sed -i "s/name='resilient_lib',/name='test_resilient_lib',/g" resilient-lib/setup.py
+sed -i "s/use_scm_version={\"root\": \"\.\.\/\", \"relative_to\": __file__},/version=\"$nextVersion\",/g" resilient-lib/setup.py
+
 sed -i "s/name='resilient_circuits',/name='test_resilient_circuits',/g" resilient-circuits/setup.py
+sed -i "s/use_scm_version={\"root\": \"\.\.\/\", \"relative_to\": __file__},/version=\"$nextVersion\",/g" resilient-circuits/setup.py
+
 sed -i "s/name='pytest_resilient_circuits',/name='test_pytest_resilient_circuits',/g" pytest-resilient-circuits/setup.py
+sed -i "s/use_scm_version={\"root\": \"\.\.\/\", \"relative_to\": __file__},/version=\"$nextVersion\",/g" pytest-resilient-circuits/setup.py
