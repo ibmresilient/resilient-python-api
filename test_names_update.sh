5b9f6022052a4d920b04be1fad20382752feabe3
Ihor-Husar <ihor.husar@ibm.com>
2020-01-17 13:18:12 -0500

    Updating names to appropriate convention

--- test_names_update.sh
+++ test_names_update.sh
@@ -1,4 +1,4 @@
-sed -i "s/name='resilient',/name='test-resilient',/g" resilient/setup.py
-sed -i "s/name='resilient-lib',/name='test-resilient-lib',/g" resilient-lib/setup.py
-sed -i "s/name='resilient-circuits',/name='test-resilient-circuits',/g" resilient-circuits/setup.py
+sed -i "s/name='resilient',/name='test_resilient',/g" resilient/setup.py
+sed -i "s/name='resilient_lib',/name='test_resilient_lib',/g" resilient-lib/setup.py
+sed -i "s/name='resilient_circuits',/name='test_resilient_circuits',/g" resilient-circuits/setup.py
 sed -i "s/name='pytest_resilient_circuits',/name='test_pytest_resilient_circuits',/g" pytest-resilient-circuits/setup.py
