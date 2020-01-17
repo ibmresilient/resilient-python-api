4ee422c3e300b17e9c9cb862f406cc4ec4d6d199
Ihor-Husar <ihor.husar@ibm.com>
2020-01-17 13:45:57 -0500

    More transparency

--- generate_git_tag.sh
+++ generate_git_tag.sh
@@ -2,14 +2,15 @@
 # If the latest tag 
 latestTag=$(git tag --list | tail -n 1)
 libVersion=$(echo $latestTag | cut -d "." -f 1,2)
-
-echo Creating tag...
+echo "Latest tag $latestTag"
 
 if [[ $latestTag =~ "dev" ]]
 then
 	git tag -a $libVersion.$BUILD_NUMBER.dev -m "$libVersion dev Build $BUILD_NUMBER"
+	echo "Creating tag... $libVersion.$BUILD_NUMBER.dev"
 else
 	git tag -a $libVersion.$BUILD_NUMBER -m "$libVersion Build $BUILD_NUMBER"
+	echo "Creating tag $libVersion.$BUILD_NUMBER"
 fi
 
 
