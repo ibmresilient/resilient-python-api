30a3d89f6e2046d77a998c5f66d2f39b7779c9ce
Ihor-Husar <ihor.husar@ibm.com>
2020-01-17 12:50:30 -0500

    fixing platfrom dependency for sec

--- test_names_update.sh
+++ test_names_update.sh
@@ -1,4 +1,4 @@
-sed -i "" "s/name='resilient',/name='test-resilient',/g" resilient/setup.py
-sed -i "" "s/name='resilient-lib',/name='test-resilient-lib',/g" resilient-lib/setup.py
-sed -i "" "s/name='resilient-circuits',/name='test-resilient-circuits',/g" resilient-circuits/setup.py
-sed -i "" "s/name='pytest_resilient_circuits',/name='test_pytest_resilient_circuits',/g" pytest-resilient-circuits/setup.py
+sed -i "" -e "s/name='resilient',/name='test-resilient',/g" resilient/setup.py
+sed -i "" -e "s/name='resilient-lib',/name='test-resilient-lib',/g" resilient-lib/setup.py
+sed -i "" -e "s/name='resilient-circuits',/name='test-resilient-circuits',/g" resilient-circuits/setup.py
+sed -i "" -e "s/name='pytest_resilient_circuits',/name='test_pytest_resilient_circuits',/g" pytest-resilient-circuits/setup.py
