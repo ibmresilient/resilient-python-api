6ce56c82378e8ae3b5cfb43cdf1dc73c051fd155
Ryan Gordon <Ryan.Gordon1@ibm.com>
2020-03-05 18:16:02 +0000

    INT-1710 Refactor code to reduce complexity

--- resilient-sdk/resilient_sdk/cmds/clone.py
+++ resilient-sdk/resilient_sdk/cmds/clone.py
@@ -5,10 +5,11 @@
 """ TODO: implement clone """
 
 import logging
+import uuid
 from resilient import ensure_unicode
 from resilient_sdk.cmds.base_cmd import BaseCmd
-from resilient_sdk.util.helpers import (get_resilient_client, get_latest_org_export, 
-                                        minify_export,get_from_export,
+from resilient_sdk.util.helpers import (get_resilient_client, get_latest_org_export,
+                                        minify_export, get_from_export,
                                         get_object_api_names)
 from resilient_sdk.util.resilient_objects import ResilientObjMap
 from resilient_sdk.util.sdk_exception import SDKException
@@ -24,11 +25,11 @@ class CmdClone(BaseCmd):
     """TODO Docstring"""
 
     CMD_NAME = "clone"
-    CMD_HELP = "Duplicate an existing Action related object (Workflow, Function) with a new api nam"
+    CMD_HELP = "Duplicate an existing Action related object (Workflow, Function) with a new api name"
     CMD_USAGE = """
     $ resilient-sdk clone --workflow <workflow_to_be_cloned> <new_workflow_name>
-    $ resilient-sdk clone -f <function_to_be_cloned> <new_function_name>
-    CMD_DESCRIPTION = "Duplicate an existing Action related object (Workflow, Function) with a new api name"""
+    $ resilient-sdk clone -f <function_to_be_cloned> <new_function_name>"""
+    CMD_DESCRIPTION = "Duplicate an existing Action related object (Workflow, Function) with a new api name"
     CMD_USE_COMMON_PARSER_ARGS = True
     CMD_ADD_PARSERS = ["res_obj_parser"]
 
@@ -84,17 +85,17 @@ class CmdClone(BaseCmd):
             else:
 
                 if args.function:
-                    self._clone_function(args, new_export_data, org_export)
+                    new_export_data['functions'] = self._clone_function(args, org_export)
 
                 if args.rule:
-                    self._clone_rule(args, new_export_data, org_export)
+                    new_export_data["actions"] = self._clone_rule(args, org_export)
 
                 if args.workflow:
-                    self._clone_workflow(args, new_export_data, org_export)
+                    new_export_data["workflows"] = self._clone_workflow(args, org_export)
 
                 if args.messagedestination:
-                    self._clone_message_destination(
-                        args, new_export_data, org_export)
+                    new_export_data["message_destinations"] = self._clone_message_destination(
+                        args, org_export)
 
             self.upload_cloned_objects(new_export_data, res_client)
 
@@ -106,13 +107,13 @@ class CmdClone(BaseCmd):
         # Import our newly cloned object with a configuration change
         try:
             result = res_client.post(IMPORT_URL, new_export_data)
-        except Exception as e:
-            LOG.error(result.text)
+        except Exception as upload_exception:
             LOG.error(new_export_data)
+
         if result["status"] == "PENDING":
             confirm_configuration_import(result, result['id'], res_client)
         else:
-            raise Exception(
+            raise SDKException(
                 "Could not import because the server did not return an import ID")
 
     def _clone_multiple_action_objects(self, args, new_export_data, org_export):
@@ -152,67 +153,65 @@ class CmdClone(BaseCmd):
         for object_type, action_objects in minified.items():
             # If the type is not fields and the value of the type is a list
             if object_type != "all_fields" and isinstance(action_objects, list):
-                # Iterate over each object in the action_objects list
-                for obj in action_objects:
-                    # If the obj is not a dict, it is not an action object
-                    if isinstance(obj, dict):
-                        old_api_name = obj.get('export_key')
-                        new_api_name = "{}_{}".format(
-                            args.prefix, old_api_name)
-                        # If the object we are dealing with was one of the requested objects
-                        # TODO: Improve spacetime complexity and make generic for all supported types
-                        if self.action_obj_was_specified(args, obj):
-                            # Handle functions
-                            if obj.get('display_name', False):
-                                new_function = replace_function_object_attrs(
-                                    obj, new_api_name)
+                # Iterate over each object in the filtered action_objects list. If the obj is not a dict, it is not an action object
+                for obj in filter(lambda obj: isinstance(obj, dict), action_objects):
+                    old_api_name = obj.get('export_key')
+                    new_api_name = "{}_{}".format(
+                        args.prefix, old_api_name)
+                    # If the object we are dealing with was one of the requested objects
+                    # TODO: Improve spacetime complexity and make generic for all supported types
+                    if self.action_obj_was_specified(args, obj):
+                        # Handle functions and datatables
+                        if obj.get('display_name', False):
+                            new_function = replace_function_object_attrs(
+                                obj, new_api_name)
 
-                                LOG.info(new_function)
-                                new_export_data['functions'].append(
-                                    new_function)
-                                # LOG.info(new_export_data)
-                            # Handle workflows
-                            elif obj.get('content', {}).get('xml', False):
-                                new_export_data['workflows'].append(replace_workflow_object_attrs(
-                                    obj, old_api_name, new_api_name, obj['name']))
-                            # Handle Message Destination. Of the supported Action Object types; only Message Destination and Workflow use programmatic_name
-                            elif obj.get('programmatic_name', False):
-                                new_export_data['message_destinations'].append(replace_md_object_attrs(
-                                    obj, new_api_name))
-    
+                            new_export_data[object_type].append(new_function)
+                        # Handle workflows
+                        elif obj.get('content', {}).get('xml', False):
+                            new_export_data['workflows'].append(replace_workflow_object_attrs(
+                                obj, old_api_name, new_api_name, obj['name']))
+                        # Handle Message Destination. Of the supported Action Object types; only Message Destination and Workflow use programmatic_name
+                        elif obj.get('programmatic_name', False):
+                            new_export_data['message_destinations'].append(replace_md_object_attrs(
+                                obj, new_api_name))
+                        # Handle Rules and everything else
+                        else:
+                            new_export_data[object_type].append(replace_rule_object_attrs(
+                                obj, new_api_name))
+ 
     @staticmethod
     def action_obj_was_specified(args, obj):
-        
-        return obj.get(ResilientObjMap.MESSAGE_DESTINATIONS, "") in args.messagedestination
+
+        supported_types = ['function', 'workflow', 'messagedestination', 'rule', 'datatable']
+        specified_objs = []
+        for type_name in supported_types:
+            # Use getattr to get each arg on the Namespace Object
+            if getattr(args, type_name):
+                specified_objs.extend(getattr(args, type_name))
+        return obj.get(ResilientObjMap.RULES, "") in specified_objs or obj.get(ResilientObjMap.WORKFLOWS, "") in specified_objs or obj.get(ResilientObjMap.FUNCTIONS, "") in specified_objs or obj.get(ResilientObjMap.DATATABLES, "") in specified_objs
 
     @staticmethod
-    def _clone_message_destination(args, new_export_data, org_export):
+    def _clone_message_destination(args, org_export):
         if len(args.messagedestination) != 2:
             raise SDKException(
                 "Only specify the original message_destination name and a new message_destination name")
         original_md_api_name, new_md_api_name = args.messagedestination
+        # Get the md defintion objects
         destinations_def = org_export.get(
             "message_destinations").copy()
         LOG.info(destinations_def)
-        # TODO: Can this be refactored?
-        duplicate_check = find_workflow_by_programmatic_name(
-            destinations_def, new_md_api_name)
-        if duplicate_check is not None:
-            raise SDKException("Workflow with the api name {} already exists".format(
-                new_md_api_name))
-        # TODO: Can this be refactoed too ?
-        original_md = find_workflow_by_programmatic_name(
-            destinations_def, original_md_api_name)
-        if original_md is None:
-            raise SDKException("Could not find original workflow {}".format(
-                original_md_api_name))
+        # Validate both the original source md exists and the new md api name does not conflict with an existing md
+        original_md = CmdClone.validate_provided_object_names("Message Destination", new_md_api_name,
+                                                              original_md_api_name, destinations_def)
+        # Update md specific items and any common obj attributes
         new_message_destination = replace_md_object_attrs(
-            original_md, new_md_api_name)
-        new_export_data["message_destinations"] = [
-            new_message_destination]
+            original_md.copy(), new_md_api_name)
+
+        return [new_message_destination]
 
     @staticmethod
-    def _clone_workflow(args, new_export_data, org_export):
+    def _clone_workflow(args, org_export):
         if len(args.workflow) != 2:
             raise Exception(
                 "Only specify the original workflow api name and a new workflow api name")
@@ -230,21 +229,20 @@ class CmdClone(BaseCmd):
         new_workflow = replace_workflow_object_attrs(
             new_workflow, original_workflow_api_name, new_workflow_api_name, old_workflow_name)
         # Add the cloned workflow to the new export object
-        new_export_data["workflows"] = [new_workflow]
-        return new_workflow_api_name, original_workflow_api_name
+        return [new_workflow]
 
     @staticmethod
     def validate_provided_object_names(obj_type_name, new_workflow_api_name, original_workflow_api_name, workflow_defs):
 
         # Perform a duplication check with the provided new_workflow_api_name
-        duplicate_check = find_workflow_by_programmatic_name(
+        duplicate_check = find_res_obj_by_identifier(
             workflow_defs, new_workflow_api_name)
 
         if duplicate_check is not None:
             raise SDKException("{} with the api name {} already exists".format(
                 obj_type_name, new_workflow_api_name))
         # Gather the original Action Object to be returned
-        original_workflow = find_workflow_by_programmatic_name(
+        original_workflow = find_res_obj_by_identifier(
             workflow_defs, original_workflow_api_name)
         # Validate the provided workflow_api_name gathered an object
         if original_workflow is None:
@@ -254,46 +252,36 @@ class CmdClone(BaseCmd):
         return original_workflow
 
     @staticmethod
-    def _clone_rule(args, new_export_data, org_export):
+    def _clone_rule(args, org_export):
         if len(args.rule) != 2:
             raise Exception(
                 "Only specify the original rule name and a new rule name")
         original_rule_api_name, new_rule_api_name = args.rule
         rule_defs = org_export.get("actions")
-        # TODO: Can this be refactored?
-        duplicate_check = find_workflow_by_programmatic_name(
-            rule_defs, new_rule_api_name)
-        if duplicate_check is not None:
-            raise Exception(
-                "Rule with the api name {} already exists".format(new_rule_api_name))
-        # TODO: Can this be refactoed too ?
-        original_rule = find_workflow_by_programmatic_name(
-            rule_defs, original_rule_api_name)
-        if original_rule is None:
-            raise Exception("Could not find original rule {}".format(
-                original_rule_api_name))
+        # Validate both the original source rule exists and the new rule api name does not conflict with an existing rule
+        original_rule = CmdClone.validate_provided_object_names("Rule", new_rule_api_name,
+                                                                original_rule_api_name, rule_defs)
         # Update workflow specific items and any common obj attributes
         new_rule = replace_rule_object_attrs(
             original_rule.copy(), new_rule_api_name)
-        # Add the cloned workflow to the new export object
-        new_export_data["actions"] = [new_rule]
+        # Return the new rule in list form
+        return [new_rule]
 
     @staticmethod
-    def _clone_function(args, new_export_data, org_export):
+    def _clone_function(args, org_export):
         if len(args.function) != 2 and not args.prefix:
             raise Exception(
                 "Only specify the original function api name and a new function api name")
         original_function_api_name, new_function_api_name = args.function
         function_defs = org_export.get("functions")
-        # Get the function we are dealing with
-        original_function = find_workflow_by_programmatic_name(
-            function_defs, original_function_api_name)
-        LOG.info(original_function)
+        # Validate both the original source function exists and the new function api name does not conflict with an existing function
+        original_function = CmdClone.validate_provided_object_names("Function", new_function_api_name,
+                                                                    original_function_api_name, function_defs)
         # Update workflow specific items and any common obj attributes
         new_function = replace_function_object_attrs(
             original_function.copy(), new_function_api_name)
         # Add the cloned workflow to the new export object
-        new_export_data["functions"] = [new_function]
+        return [new_function]
 
 
 def confirm_configuration_import(result, import_id, res_client):
@@ -301,16 +289,18 @@ def confirm_configuration_import(result, import_id, res_client):
         result["status"] = "ACCEPTED"      # Have to confirm changes
         uri = "/configurations/imports/{}".format(import_id)
         res_client.put(uri, result, timeout=5)
-        LOG.info("Imported successfully")
-    except Exception as e:
-        LOG.error(result)
-        LOG.error(repr(e))
+        LOG.info("Imported configuration changes successfully")
+    except Exception as import_exception:
+        raise SDKException(repr(import_exception))
 
 
-def find_workflow_by_programmatic_name(workflows, pname):
-    for workflow in workflows:
-        if workflow.get("programmatic_name") == pname or workflow.get("export_key") == pname:
-            return workflow
+def find_res_obj_by_identifier(objects_list, identifier_value):
+    for action_object in objects_list:
+        if action_object.get(ResilientObjMap.DATATABLES) == identifier_value \
+                or action_object.get(ResilientObjMap.FUNCTIONS) == identifier_value \
+                or action_object.get(ResilientObjMap.RULES) == identifier_value \
+                or action_object.get(ResilientObjMap.WORKFLOWS) == identifier_value:
+            return action_object
 
     return None
 
@@ -320,7 +310,7 @@ def replace_common_object_attrs(obj_to_modify, new_obj_api_name):
     When cloning an Action Object, depending on the type of that object certain fields will need to be overwritten.
     In most cases the name, export_key and uuid will always need to be changed to prevent duplication errors.
 
-    :param obj_to_modify: The Action Object we are replacing values for 
+    :param obj_to_modify: The Action Object we are replacing values for
     :type obj_to_modify: dict
     :param original_obj_api_name: the API name of the Action Object we are cloning
     :type original_obj_api_name: string
@@ -329,7 +319,6 @@ def replace_common_object_attrs(obj_to_modify, new_obj_api_name):
     :return: A modified Action Object which can be imported to Resilient
     :rtype: dict
     """
-    import uuid
     obj_to_modify.update(
         {
             "uuid": str(uuid.uuid4()),
@@ -369,6 +358,10 @@ def replace_function_object_attrs(obj_to_modify, new_obj_api_name):
         "display_name": new_obj_api_name,
         ResilientObjMap.FUNCTIONS: new_obj_api_name
     })
+    if obj_to_modify.get(ResilientObjMap.DATATABLES, False):
+        obj_to_modify.update({
+            ResilientObjMap.DATATABLES: new_obj_api_name
+        })
     return obj_to_modify
 
 
