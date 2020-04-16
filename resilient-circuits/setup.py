edad4172bf91da5a1118b4456649ad0fff5d77aa
John Prendergast <johnpren@ie.ibm.com>
2020-04-16 11:23:54 +0100

    INT-2486 - Bump dependency of resilient to 36.2 for resilient circuits.

--- resilient-circuits/setup.py
+++ resilient-circuits/setup.py
@@ -13,7 +13,7 @@ import io
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-requires_resilient_version = "35.0"
+requires_resilient_version = "36.2"
 major, minor = requires_resilient_version.split('.', 2)[:2]
 
 
