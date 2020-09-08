706631ea39fbd90dcba9e2359a0a12d9cdbe84de
Mark-Scherfling <mark.scherfling@ibm.com>
2020-09-08 13:46:11 -0400

    INT-2961 - updated install-requires for setuptools

--- resilient/setup.py
+++ resilient/setup.py
@@ -120,7 +120,7 @@ setup(
         'requests-mock>=1.2.0',
         'six',
         'cachetools<3.0.0',
-        'setuptools>=41.0.0,<50.0',
+        'setuptools>=41.0.0,<50.0,>50.0'
     ],
     extras_require={
         ':python_version < "3.2"': [
