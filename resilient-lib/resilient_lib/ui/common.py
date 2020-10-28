14186a48b4a20a7eb95ba09915f6e9470e76259d
Ihor-Husar <ihor.husar@ibm.com>
2020-10-28 15:48:25 -0400

    Indentation using spaces

--- resilient-lib/resilient_lib/ui/common.py
+++ resilient-lib/resilient_lib/ui/common.py
@@ -17,98 +17,98 @@ INCIDENT_TABS_NAME = "incident"
 UI_LOCK = "ui_lock"
 
 def get_organization_layout(client, layout_name):
-	# first get a type ID for layout UI
-	types = client.get(TYPES_URL)
-	if ORGANIZATION_TYPE not in types:
-		raise KeyError("Can't find 'organization' in the list of types.")
+    # first get a type ID for layout UI
+    types = client.get(TYPES_URL)
+    if ORGANIZATION_TYPE not in types:
+        raise KeyError("Can't find 'organization' in the list of types.")
 
-	organization = types[ORGANIZATION_TYPE]
-	organization_type_id = organization["type_id"]
-	# for organization extract UI layouts
-	incident_layout = client.get(LAYOUT_FOR_TYPE_QUERY.format(organization_type_id))
-	incident_tabs = [layout for layout in incident_layout if layout['name'] == layout_name]
+    organization = types[ORGANIZATION_TYPE]
+    organization_type_id = organization["type_id"]
+    # for organization extract UI layouts
+    incident_layout = client.get(LAYOUT_FOR_TYPE_QUERY.format(organization_type_id))
+    incident_tabs = [layout for layout in incident_layout if layout['name'] == layout_name]
 
-	if not incident_tabs:
-		raise ValueError("No UI tabs are accessible")
-	elif len(incident_tabs) != 1:
-		raise ValueError("Incorrect number of UI tabs supplied") 
+    if not incident_tabs:
+        raise ValueError("No UI tabs are accessible")
+    elif len(incident_tabs) != 1:
+        raise ValueError("Incorrect number of UI tabs supplied") 
 
-	return incident_tabs[0]
+    return incident_tabs[0]
 
 
 def get_incident_layout(client):
-	return get_organization_layout(client, INCIDENT_TABS_NAME)
+    return get_organization_layout(client, INCIDENT_TABS_NAME)
 
 def get_incident_tabs(client):
-	"""
-	Get a list of incident tabs exposed to user.
-	:param client: Resilient client
-	:returns: a tuple of layout id for incident UI and current list of UI tabs
-	"""
-	
-	tab_data = get_incident_layout(client)
-	return tab_data.get('content')
+    """
+    Get a list of incident tabs exposed to user.
+    :param client: Resilient client
+    :returns: a tuple of layout id for incident UI and current list of UI tabs
+    """
+    
+    tab_data = get_incident_layout(client)
+    return tab_data.get('content')
 
 def add_tab_to_layout(client, layout, new_tab):
-	layout = copy.deepcopy(layout)
-	layout['content'].append(
-		new_tab
-	)
-	return client.put(LAYOUT_FOR.format(layout['id']), payload=layout)
+    layout = copy.deepcopy(layout)
+    layout['content'].append(
+        new_tab
+    )
+    return client.put(LAYOUT_FOR.format(layout['id']), payload=layout)
 
 def update_tab(client, layout, tab):
-	"""
-	Needs to get the tab and add missing fields to it.
-	"""
-	layout = copy.deepcopy(layout)
-	missing_fields = tab.get_missing_fields(layout.get("content"))
-	print("missing fields")
-	if not len(missing_fields):
-		return None
-	tab_data = tab.get_from_tabs(layout.get("content"))
-	tab_data['fields'].extend(missing_fields)
+    """
+    Needs to get the tab and add missing fields to it.
+    """
+    layout = copy.deepcopy(layout)
+    missing_fields = tab.get_missing_fields(layout.get("content"))
+    print("missing fields")
+    if not len(missing_fields):
+        return None
+    tab_data = tab.get_from_tabs(layout.get("content"))
+    tab_data['fields'].extend(missing_fields)
 
-	return client.put(LAYOUT_FOR.format(layout['id']), payload=layout)
+    return client.put(LAYOUT_FOR.format(layout['id']), payload=layout)
 
 def permission_to_edit(tab, opts):
-	"""
-	Gets the config and determines if the UI had been locked or is allowed to be edited.
-	:param tab: class or instance that's a subclass of ui.Tab
-	:return: If app.config locks the tab or not
-	"""
-	if opts.get(tab.SECTION):
-		section_lock = get_config_boolean(opts.get(tab.SECTION).get(UI_LOCK))
-		return not section_lock
-	if opts.get('integrations'):
-		integrations_lock = get_config_boolean(opts.get('integrations').get(UI_LOCK))
-		return not integrations_lock
-	if opts.get('resilient'):
-		global_lock = get_config_boolean(opts.get('resilient').get(UI_LOCK))
-		return not global_lock
+    """
+    Gets the config and determines if the UI had been locked or is allowed to be edited.
+    :param tab: class or instance that's a subclass of ui.Tab
+    :return: If app.config locks the tab or not
+    """
+    if opts.get(tab.SECTION):
+        section_lock = get_config_boolean(opts.get(tab.SECTION).get(UI_LOCK))
+        return not section_lock
+    if opts.get('integrations'):
+        integrations_lock = get_config_boolean(opts.get('integrations').get(UI_LOCK))
+        return not integrations_lock
+    if opts.get('resilient'):
+        global_lock = get_config_boolean(opts.get('resilient').get(UI_LOCK))
+        return not global_lock
 
-	return True
+    return True
 
 def create_tab(tab, update_existing=False):
-	opts = get_opts()
-	if not permission_to_edit(tab, opts):
-		LOG.info("No permission to edit UI for {}".format(tab.SECTION))
-		return
-	client = resilient.get_client(opts)
-	layout = get_incident_layout(client)
+    opts = get_opts()
+    if not permission_to_edit(tab, opts):
+        LOG.info("No permission to edit UI for {}".format(tab.SECTION))
+        return
+    client = resilient.get_client(opts)
+    layout = get_incident_layout(client)
 
-	# check if tab already exists in the layout
-	if tab.exists_in(layout.get("content")):
-		if update_existing:
-			LOG.info("UI tab for {} already exists. Checking for updates.".format(tab.SECTION))
-			return update_tab(client, layout, tab)
-		else:
-			LOG.info("UI tab for {} already exists. Not updating.".format(tab.SECTION))
-			return
-	LOG.info("Creating a UI tab for {}".format(tab.SECTION))
-	return add_tab_to_layout(client, layout, tab.as_dto())
+    # check if tab already exists in the layout
+    if tab.exists_in(layout.get("content")):
+        if update_existing:
+            LOG.info("UI tab for {} already exists. Checking for updates.".format(tab.SECTION))
+            return update_tab(client, layout, tab)
+        else:
+            LOG.info("UI tab for {} already exists. Not updating.".format(tab.SECTION))
+            return
+    LOG.info("Creating a UI tab for {}".format(tab.SECTION))
+    return add_tab_to_layout(client, layout, tab.as_dto())
 
 def get_opts():
-	config = resilient.get_config_file()
-	return AppArgumentParser(config_file=config).parse_args()
+    config = resilient.get_config_file()
+    return AppArgumentParser(config_file=config).parse_args()
 
 
--- resilient-lib/resilient_lib/ui/tab.py
+++ resilient-lib/resilient_lib/ui/tab.py
@@ -7,61 +7,61 @@ UI_TAB_ELEMENT = "tab"
 UI_TAB_FIELD_TYPE = "incident"
 
 class RequiredTabFields(type):
-	def __init__(cls, name, bases, attrs):
-		if not bases:
-			return  # No bases implies Tab, which doesn't need to define the properties
-		for attr in ["UUID", "NAME", "SECTION", "CONTAINS"]:
-			if not hasattr(cls, attr) or getattr(cls, attr) is None:
-				raise AttributeError("{} is missing from class definition of a Tab".format(attr))
-	
+    def __init__(cls, name, bases, attrs):
+        if not bases:
+            return  # No bases implies Tab, which doesn't need to define the properties
+        for attr in ["UUID", "NAME", "SECTION", "CONTAINS"]:
+            if not hasattr(cls, attr) or getattr(cls, attr) is None:
+                raise AttributeError("{} is missing from class definition of a Tab".format(attr))
+    
 
 class Tab(six.with_metaclass(RequiredTabFields)):
-	UUID = None
-	NAME = None
-	SECTION = None
-	
+    UUID = None
+    NAME = None
+    SECTION = None
+    
 
-	CONTAINS = None
+    CONTAINS = None
 
-	@classmethod
-	def exists(cls, client):
-		"""
-		Check if the tab exists in Resilient
-		"""
-		return cls.exists_in(get_incident_tabs(client))
+    @classmethod
+    def exists(cls, client):
+        """
+        Check if the tab exists in Resilient
+        """
+        return cls.exists_in(get_incident_tabs(client))
 
-	@classmethod
-	def exists_in(cls, tabs):
-		"""
-		Checks if the tab exists in the list of tabs.
-		"""
-		return cls.get_from_tabs(tabs) is not None
+    @classmethod
+    def exists_in(cls, tabs):
+        """
+        Checks if the tab exists in the list of tabs.
+        """
+        return cls.get_from_tabs(tabs) is not None
 
-	@classmethod
-	def get_from_tabs(cls, tabs):
-		return next((x for x in tabs if x.get("predefined_uuid") == cls.UUID), None)
+    @classmethod
+    def get_from_tabs(cls, tabs):
+        return next((x for x in tabs if x.get("predefined_uuid") == cls.UUID), None)
 
-	@classmethod
-	def as_dto(cls):
-		return {
-			"step_label": cls.NAME,
-			"fields": [field.as_dto() for field in cls.CONTAINS] if cls.CONTAINS else [],
-			"show_if": [],
-			"element": UI_TAB_ELEMENT,
-			"field_type": UI_TAB_FIELD_TYPE,
-			"predefined_uuid": cls.UUID,
-			"show_link_header": False
-		}
+    @classmethod
+    def as_dto(cls):
+        return {
+            "step_label": cls.NAME,
+            "fields": [field.as_dto() for field in cls.CONTAINS] if cls.CONTAINS else [],
+            "show_if": [],
+            "element": UI_TAB_ELEMENT,
+            "field_type": UI_TAB_FIELD_TYPE,
+            "predefined_uuid": cls.UUID,
+            "show_link_header": False
+        }
 
-	@classmethod
-	def get_missing_fields(cls, tabs):
-		"""
-		Given all the tabs
-		"""
-		if not cls.exists_in(tabs):
-			return False
-		tab = cls.get_from_tabs(tabs)
-		tab_fields = tab.get('fields', [])
-		return [field.as_dto() for field in cls.CONTAINS if not field.exists_in(tab_fields)]
-		
+    @classmethod
+    def get_missing_fields(cls, tabs):
+        """
+        Given all the tabs
+        """
+        if not cls.exists_in(tabs):
+            return False
+        tab = cls.get_from_tabs(tabs)
+        tab_fields = tab.get('fields', [])
+        return [field.as_dto() for field in cls.CONTAINS if not field.exists_in(tab_fields)]
+        
 
