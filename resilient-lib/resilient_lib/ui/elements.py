10b0f78bdbc93d1367aeaae0ea28d87c37fa9773
Ihor-Husar <ihor.husar@ibm.com>
2020-10-28 15:47:34 -0400

    Converting indentation to spaces

--- resilient-lib/resilient_lib/ui/elements.py
+++ resilient-lib/resilient_lib/ui/elements.py
@@ -1,41 +1,39 @@
 class UIElementBase(object):
-	ELEMENT_TYPE = None
+    ELEMENT_TYPE = None
 
-	def __init__(self, api_name):
-		assert self.ELEMENT_TYPE, "Element type for subclass needs to be defined"
-		self.api_name = api_name
+    def __init__(self, api_name):
+        assert self.ELEMENT_TYPE, "Element type for subclass needs to be defined"
+        self.api_name = api_name
 
-	def as_dto(self):
-		"""
-		Returns a JSON serializable dictionary with DTO representing the field in the tab.
-		"""
-		raise NotImplemenetedError("DTO represenation for {} is not implemented".format(self.__class__))
+    def as_dto(self):
+        """
+        Returns a JSON serializable dictionary with DTO representing the field in the tab.
+        """
+        raise NotImplemenetedError("DTO represenation for {} is not implemented".format(self.__class__))
 
-	def exists_in(self, fields):
-		"""
-		Given a list of fields of a tab, find if current one is one of them.
-		"""
-		return next((field for field in fields if field["element"] == self.ELEMENT_TYPE and field["content"] == self.api_name), None) is not None
+    def exists_in(self, fields):
+        """
+        Given a list of fields of a tab, find if current one is one of them.
+        """
+        return next((field for field in fields if field["element"] == self.ELEMENT_TYPE and field["content"] == self.api_name), None) is not None
 
 
 class Field(UIElementBase):
-	ELEMENT_TYPE = "field"
+    ELEMENT_TYPE = "field"
 
-	def as_dto(self):
-		return {
-			"content": self.api_name,
-			"element": self.ELEMENT_TYPE,
-			"field_type": "incident"
-		}
+    def as_dto(self):
+        return {
+            "content": self.api_name,
+            "element": self.ELEMENT_TYPE,
+            "field_type": "incident"
+        }
 
 
 class Datatable(UIElementBase):
-	ELEMENT_TYPE = "datatable"
+    ELEMENT_TYPE = "datatable"
 
-	def as_dto(self):
-		return {
-			"element": self.ELEMENT_TYPE,
-			"content": self.api_name
-		}
-
-
+    def as_dto(self):
+        return {
+            "element": self.ELEMENT_TYPE,
+            "content": self.api_name
+        }
