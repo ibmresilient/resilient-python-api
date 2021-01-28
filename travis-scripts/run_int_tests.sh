70162a82397871fb3fef3c4e49ed1cadf50df128
Shane Curtin <shane.curtin@ie.ibm.com>
2021-01-28 14:59:28 +0000

    INT-3376: commit mock_app_config [run-int-tests]

--- travis-scripts/run_int_tests.sh
+++ travis-scripts/run_int_tests.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -e
 
-PATH_MOCK_APP_CONFIG="$TRAVIS_BUILD_DIR/travis-scripts/mock_app.config"
+PATH_MOCK_APP_CONFIG="$TRAVIS_BUILD_DIR/travis-scripts/mock_app_config"
 
 sed -e "s|{{FYRE_CLUSTER_DOMAIN}}|myresilient1.fyre.ibm.com|" \
 -e "s|{{FYRE_CLUSTER_API_KEY_ID}}|$FYRE_CLUSTER_API_KEY_ID|" \
 -e "s|{{FYRE_CLUSTER_API_KEY_SECRET}}|$FYRE_CLUSTER_API_KEY_SECRET|" \
 $PATH_MOCK_APP_CONFIG > $TRAVIS_BUILD_DIR/mock_app.config
+
+cat $TRAVIS_BUILD_DIR/mock_app.config
 
 tox -c ./resilient-sdk -e INT
