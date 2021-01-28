89d3bbd9c590183e4f40f057e7f7b06ed62c46b6
Shane Curtin <shane.curtin@ie.ibm.com>
2021-01-28 16:29:56 +0000

    INT-3376: Add after_script to delete FYRE cluster - [run-int-tests]

--- travis-scripts/run_int_tests.sh
+++ travis-scripts/run_int_tests.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -e
 
-PATH_MOCK_APP_CONFIG="$TRAVIS_BUILD_DIR/travis-scripts/mock_app_config"
-
+# Generating mock app.config
 sed -e "s|{{FYRE_CLUSTER_DOMAIN}}|$FYRE_CLUSTER_DOMAIN|" \
 -e "s|{{FYRE_CLUSTER_API_KEY_ID}}|$FYRE_CLUSTER_API_KEY_ID|" \
 -e "s|{{FYRE_CLUSTER_API_KEY_SECRET}}|$FYRE_CLUSTER_API_KEY_SECRET|" \
