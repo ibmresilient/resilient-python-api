e6cb6393a74031bb1c771ee18ab4c0b0d4176b7b
Shane Curtin <shane.curtin@ie.ibm.com>
2021-02-26 17:35:15 +0000

    INT-3507: Add resilient-lib dependency

--- resilient-circuits/setup.py
+++ resilient-circuits/setup.py
@@ -58,7 +58,6 @@ setup(
     author='IBM Resilient',
     install_requires=[
         'stompest>=2.3.0',
-        'requests>=2.6.0',
         'circuits',
         'pytz',
         'jinja2>=2.10.0',
@@ -66,7 +65,8 @@ setup(
         'filelock>=2.0.5',
         'watchdog>=0.9.0, <1.0.0; python_version < "3.6.0"',
         'watchdog>=0.9.0; python_version >= "3.6.0"',
-        'resilient>=38.0.0'
+        'resilient>=38.0.0',
+        'resilient-lib>=40.0.0'
     ],
     author_email='support@resilientsystems.com',
     description='Resilient Circuits Framework for Custom Integrations',
