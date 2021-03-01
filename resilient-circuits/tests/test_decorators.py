235ebf259b2307abdb640ce58f2d24daae743d2e
Shane Curtin <shane.curtin@ie.ibm.com>
2021-03-01 12:59:34 +0000

    INT-3507: Add common code to call test fn - Added more unit tests for 'app_function' decorator

--- resilient-circuits/tests/test_decorators.py
+++ resilient-circuits/tests/test_decorators.py
@@ -2,97 +2,84 @@
 # -*- coding: utf-8 -*-
 # (c) Copyright IBM Corp. 2010, 2021. All Rights Reserved.
 
-import pytest
-from resilient_circuits import ResilientComponent, app_function, StatusMessage, FunctionResult, FunctionMessage, SubmitTestFunction
-from circuits import Event
 import os
+import pytest
+from circuits import Event
+from mock import patch
+from resilient_lib import IntegrationError
+from resilient_circuits import ResilientComponent, app_function, StatusMessage, FunctionResult, SubmitTestFunction
+from tests import helpers
 
-# TODO: global vars for PACKAGE_NAME and FN_NAME
 
-resilient_mock = "pytest_resilient_circuits.BasicResilientMock"
+MOCK_PACKAGE_NAME = u"mock_function_package"
+MOCK_FN_NAME = u"mock_function"
+resilient_mock = u"pytest_resilient_circuits.BasicResilientMock"
 
-config_data = """[mock_function]
-url = https://www.example.com"""
+config_data = """[{0}]
+url = https://www.example.com""".format(MOCK_PACKAGE_NAME)
 
 mock_opts = {
-    "resilient_mock": "pytest_resilient_circuits.BasicResilientMock",
+    "host": "192.168.0.1",
     "org": "Test Organization",
     "email": "admin@example.com",
     "password": "123",
-    "host": "192.168.0.1"
+    "resilient_mock": resilient_mock
 }
 
 
 class MockComponent(ResilientComponent):
 
-    PACKAGE_NAME = "mock_function"
+    PACKAGE_NAME = MOCK_PACKAGE_NAME
 
-    @app_function("mock_function")
+    @app_function(MOCK_FN_NAME)
     def mock_function(self, fn_inputs, **kwargs):
-        yield StatusMessage("Mock StatusMessage 1")
-        yield StatusMessage("Mock StatusMessage 2")
+        yield StatusMessage(u"Mock զ է ը թ ժ ի լ StatusMessage 1")
+        yield StatusMessage(u"Mock StatusMessage 2")
         yield FunctionResult({"malware": True})
 
-
-def call_fn(circuits_app):
-
-    # Create the submitTestFunction event
-    evt = SubmitTestFunction("mock_function", {"input_one": "abc"})
-
-    # Fire a message to the function
-    circuits_app.manager.fire(evt)
-
-    # circuits will fire an "exception" event if an exception is raised in the ResilientComponent
-    # return this exception if it is raised
-    exception_event = circuits_app.watcher.wait("exception", parent=None, timeout=2)
-
-    if exception_event is not False:
-        exception = exception_event.args[1]
-        raise exception
-
-    # else return the ResilientComponent's results
-    else:
-        event = circuits_app.watcher.wait("mock_function_result", parent=evt, timeout=2)
-        assert event
-        assert isinstance(event.kwargs["result"], FunctionResult)
-        pytest.wait_for(event, "complete", True)
-        return event.kwargs["result"].value
+    @app_function(MOCK_FN_NAME + u"raise_exception")
+    def mock_function_raise_exception(self, fn_inputs, **kwargs):
+        raise IntegrationError(u"mock error message with unicode զ է ը թ ժ ի լ խ")
 
 
 def test_app_function_basic_decoration():
     assert MockComponent.mock_function.handler is True
     assert MockComponent.mock_function.function is True
-    assert MockComponent.mock_function.names == ("mock_function",)
+    assert MockComponent.mock_function.names == (MOCK_FN_NAME,)
     assert MockComponent.mock_function.priority == 0
-    assert MockComponent.mock_function.channel == "functions.mock_function"
+    assert MockComponent.mock_function.channel == u"functions." + MOCK_FN_NAME
     assert MockComponent.mock_function.override is False
     assert MockComponent.mock_function.event is True
 
 
-def test_app_function_validates_fn_inputs():
-    # TODO
-    pass
-
-
-def test_app_function_runs_2(circuits_app):
-    # TODO
+def test_app_function_runs(circuits_app):
     MockComponent(opts=mock_opts).register(circuits_app.app.component_loader)
-    mock_results = call_fn(circuits_app)
+    helpers.call_fn(MOCK_FN_NAME, {"input_one": "abc"}, circuits_app)
 
 
-def test_app_function_handles_FunctionResult():
-    # TODO
-    pass
+def test_app_function_handles_FunctionResult(circuits_app):
+    mock_fn_inputs = {"input_one": u"abc", "input_two": u"unicode ઠ ડ ઢ ણ ત થ દ ધ ન પ ફ input"}
+    MockComponent(opts=mock_opts).register(circuits_app.app.component_loader)
+    mock_results = helpers.call_fn(MOCK_FN_NAME, mock_fn_inputs, circuits_app)
+
+    assert mock_results["version"] == "1.0"
+    assert mock_results["success"] is True
+    assert mock_results["reason"] is None
+    assert mock_results["inputs"]["input_one"] == mock_fn_inputs["input_one"]
+    assert mock_results["inputs"]["input_two"] == mock_fn_inputs["input_two"]
+    assert mock_results["content"]["malware"] is True
 
 
-def test_app_function_handles_StatusMessage():
-    # TODO
-    pass
+def test_app_function_handles_StatusMessage(circuits_app):
+    MockComponent(opts=mock_opts).register(circuits_app.app.component_loader)
+    mock_status_message = helpers.call_fn(MOCK_FN_NAME, {"input_one": "abc"}, circuits_app, status_message_only=True)
+    assert mock_status_message.text == u"Mock զ է ը թ ժ ի լ StatusMessage 1"
 
 
-def test_app_function_handles_Exception():
-    # TODO
-    pass
+def test_app_function_handles_Exception(circuits_app):
+    MockComponent(opts=mock_opts).register(circuits_app.app.component_loader)
+    with pytest.raises(IntegrationError, match=r"mock error message with unicode զ է ը թ ժ ի լ խ"):
+        helpers.call_fn(MOCK_FN_NAME + u"raise_exception", {"input_one": "abc"}, circuits_app)
 
 
 def test_too_many_function_names():
