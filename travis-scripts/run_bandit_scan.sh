360db1967c94d5c8436320d5d9e265063548153b
Ryan Gordon <Ryan.Gordon1@ibm.com>
2021-03-05 12:58:19 +0000

    INT-3564 Update to use bandit scan param

--- travis-scripts/run_bandit_scan.sh
+++ travis-scripts/run_bandit_scan.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -e
-RCFILE="./travis-scripts/travis-configs/banditconfig.yaml "
+RCFILE="./travis-scripts/travis-configs/banditconfig.yaml"
 # Run a Bandit scan on all packages
 # Bandit configurgation is pulled from a yaml file
-bandit -r . --configfile $RCFILE
+
 # TODO: Bandit gives us information on vulnerbilities that could exist today
 # if we depend on its exit code the builds will fail until we fix or ignore the specific instances
-# for this reason, exit 0 to give the team information on things to change.
-# Should be removed.
-exit 0 
+# for this reason, --exit-zero to give the team information on things to change.
+# --exit-zero should be removed at a later point.
+bandit -r . --configfile $RCFILE --exit-zero
+
