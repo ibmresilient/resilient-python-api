56eb6458d17913f06601ba68302bd0b752b98c45
John Prendergast <johnpren@ie.ibm.com>
2021-04-07 13:00:08 +0100

    INT-3638 - Always regenerate workflow data files.

--- resilient-sdk/resilient_sdk/cmds/codegen.py
+++ resilient-sdk/resilient_sdk/cmds/codegen.py
@@ -137,20 +137,30 @@ class CmdCodegen(BaseCmd):
                         export_target_file = os.path.join(target_dir, u"{0}{1}".format(export_obj_name, target_ext))
 
                 if os.path.exists(target_file):
-                    files_skipped.append(os.path.relpath(target_file, start=package_dir))
-                    continue
+                    # Don't skip for workflows.
+                    if target_ext != ".md" or not export_target_file:
+                        files_skipped.append(os.path.relpath(target_file, start=package_dir))
+                        continue
 
                 # Check for possible target_files created from earlier versions of codegen.
                 if export_target_file and os.path.exists(export_target_file):
-                    files_skipped.append(os.path.relpath(export_target_file, start=package_dir))
-                    continue
+                    # Don't skip for workflows.
+                    if target_ext != ".md":
+                        files_skipped.append(os.path.relpath(export_target_file, start=package_dir))
+                        continue
 
                 jinja_template = jinja_env.get_template(path_template)
                 jinja_rendered_text = jinja_template.render(template_data)
 
-                newly_generated_files.append(os.path.relpath(target_file, start=package_dir))
+                if export_target_file and os.path.exists(export_target_file):
+                    # Overwrite file e.g. workflow data created from earlier versions of codegen.
+                    write_target_file = export_target_file
+                else:
+                    write_target_file = target_file
 
-                sdk_helpers.write_file(target_file, jinja_rendered_text)
+                newly_generated_files.append(os.path.relpath(write_target_file, start=package_dir))
+
+                sdk_helpers.write_file(write_target_file, jinja_rendered_text)
 
         return newly_generated_files, files_skipped
 
