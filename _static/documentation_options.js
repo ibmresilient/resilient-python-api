e5d9723ad7325b78ab86900506615f44537b47a8
Shane Curtin (via Travis CI) <shane.curtin@ie.ibm.com>
2022-02-24 14:52:10 +0000

    Deploy Resilient/resilient-python-api to github.ibm.com/Resilient/resilient-python-api.git:gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '44.0.2739',
+    VERSION: '44.0.2761',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
