19e1fed7efe11b045ea5533b075c375d6884b6de
Shane Curtin <shane.curtin@ie.ibm.com>
2022-03-31 16:32:25 +0100

    INT-4462: improve log message

--- resilient/resilient/constants.py
+++ resilient/resilient/constants.py
@@ -17,4 +17,4 @@ ERROR_CODE_CONNECTION_UNAUTHORIZED = 21
 
 # Error Messages
 ERROR_MSG_CONNECTION_UNAUTHORIZED = u"Unauthorized"
-ERROR_MSG_CONNECTION_INVALID_CREDS = u"API Credentials incorrect. Note: the API Key may be blocked and need to be regenerated"
+ERROR_MSG_CONNECTION_INVALID_CREDS = u"Either the API Key has been blocked, the API Credentials are incorrect or the IP address has been banned. Please review the SOAR logs for more information"
