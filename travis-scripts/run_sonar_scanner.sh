6d88e71be4a9031213e4e03d705746495852517a
Shane Curtin <shane.curtin@ie.ibm.com>
2022-05-06 15:49:41 +0100

    INT-4032 {build} add SONAR_QUBE_SEC_TO_WAIT_FOR_ANALYSIS var

--- travis-scripts/run_sonar_scanner.sh
+++ travis-scripts/run_sonar_scanner.sh
@@ -12,5 +12,9 @@ $PATH_SONAR_PROPERTIES > $PATH_SONAR_PROPERTIES.tmp && mv $PATH_SONAR_PROPERTIES
 # Run the sonar-scanner
 /tmp/sonar-scanner-$SONAR_SCANNER_VERSION-linux/bin/sonar-scanner -Dsonar.host.url=$SONAR_QUBE_URL -Dsonar.login=$SONAR_QUBE_TOKEN
 
+# Sleep here to ensure there is sufficient time for analysis report
+# to be generated. This is a Travis env var and can be adjusted
+sleep $SONAR_QUBE_SEC_TO_WAIT_FOR_ANALYSIS
+
 # Get analysis status
 python $PATH_SCRIPTS_DIR/get_sonarqube_project_status.py "$SONAR_QUBE_URL" "$SONAR_QUBE_TOKEN" "$SONAR_QUBE_PROJ_KEY" "$TRAVIS_BRANCH"
