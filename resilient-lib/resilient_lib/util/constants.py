9d39a04eb97c29107de5b070400f58f029d95640
Bo Bleckel <bo.bleckel@ibm.com>
2022-05-13 11:07:05 -0400

    INT-4760 {resilient-lib} update ignore

--- resilient-lib/resilient_lib/util/constants.py
+++ resilient-lib/resilient_lib/util/constants.py
@@ -2,7 +2,5 @@
 # -*- coding: utf-8 -*-
 # (c) Copyright IBM Corp. 2010, 2022. All Rights Reserved
 
-# pragma: no cover
-
 # resilient_common logging constants
 WARN_BUILD_INCIDENT_ORG_ID = "Building an incident link without the ORG ID may produce a link that directs the user to a different incident resulting in unexpected results. It is recommended to provide the ORG ID."
