65167fe60a2d4b98d8e41c58a263b6136093d7ad
Mark Scherfling <mark.scherfling@ibm.com>
2022-08-12 09:15:31 -0400

    INT-5092 {resilient_lib} missed checking

--- resilient-lib/resilient_lib/ui/__init__.py
+++ resilient-lib/resilient_lib/ui/__init__.py
@@ -3,4 +3,4 @@
 # pragma pylint: disable=unused-argument, no-self-use
 from .common import create_tab
 from .tab import Tab
-from .elements import Datatable, Field
+from .elements import Datatable, Field, View
