2dd96ef8e401fbd3c1e8c66859d240d500aa391f
Bo Bleckel <bo.bleckel@ibm.com>
2022-10-24 11:23:20 -0400

    SOARAPPS-132 {resilient-circuits} update default num workers

--- resilient-circuits/resilient_circuits/validate_configs.py
+++ resilient-circuits/resilient_circuits/validate_configs.py
@@ -2,13 +2,13 @@
 # -*- coding: utf-8 -*-
 # (c) Copyright IBM Corp. 2010, 2020. All Rights Reserved.
 
-from .constants import MAX_NUM_WORKERS
+from .constants import MIN_NUM_WORKERS, MAX_NUM_WORKERS
 """Contains a dict to validate the app configs"""
 
 VALIDATE_DICT = {
     "num_workers": {
         "required": False,
-        "valid_condition": lambda c: True if c >= 1 and c <= MAX_NUM_WORKERS else False,
-        "invalid_msg": "num_workers must be in the range 1 <= {}".format(MAX_NUM_WORKERS)
+        "valid_condition": lambda c: True if c >= MIN_NUM_WORKERS and c <= MAX_NUM_WORKERS else False,
+        "invalid_msg": "num_workers must be in the range {} <= {}".format(MIN_NUM_WORKERS, MAX_NUM_WORKERS)
     }
 }
