2cb7a5b35fe9eccdd8f9d0ac36e0bb7e8f500c0d
Bo Bleckel (via Travis CI) <Bo.Bleckel@ibm.com>
2022-12-12 19:26:47 +0000

    Deploy Resilient/resilient-python-api to github.ibm.com/Resilient/resilient-python-api.git:gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '47.1.3777',
+    VERSION: '47.1.3779',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
