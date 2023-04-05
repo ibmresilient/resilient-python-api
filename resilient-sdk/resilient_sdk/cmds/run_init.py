5f6c5cf31c0b43f9b0f43337678724d057128dd0
Priya-Sapra <priya.sapra@ibm.com>
2023-04-05 14:57:25 -0400

    SOARAPPS-6495 {resilient-sdk} add more options for different fields

--- resilient-sdk/resilient_sdk/cmds/run_init.py
+++ resilient-sdk/resilient_sdk/cmds/run_init.py
@@ -7,6 +7,7 @@
 import logging
 import os
 
+from argparse import SUPPRESS
 from resilient import ensure_unicode
 from resilient_sdk.cmds.base_cmd import BaseCmd
 from resilient_sdk.util import constants
@@ -25,12 +26,12 @@ class CmdRunInit(BaseCmd):
     for `codegen`, `docgen`, and `validate` commands
     """
 
-    # TODO: add an option to provide path or to provide name?
-
     CMD_NAME = "init"
     CMD_HELP = "Generates sdk_settings.json to store default settings."
     CMD_USAGE = """
     $ resilient-sdk init
+    $ resilient-sdk init -f/--file <path to settings json>
+    $ resilient-sdk init -f/--file <path to settings json> -a/--author you@example.com
     """
     CMD_DESCRIPTION = CMD_HELP
     CMD_ADD_PARSERS = [constants.SDK_SETTINGS_PARSER_NAME]
@@ -40,13 +41,42 @@ class CmdRunInit(BaseCmd):
         self.parser.usage = self.CMD_USAGE
         self.parser.description = self.CMD_DESCRIPTION
 
-        # Add any positional or optional arguments here
+        # Argument to specify a filepath
         self.parser.add_argument("-f", "--file",
                                  type=ensure_unicode,
-                                 required = False,
+                                 required=False,
                                  help="Optional path to settings file.")
         
+        # Optional args for different fields in the settings file
+        # TODO: Would it be better to allow something like this instead of individual arguments?
+        ### $ resilient-sdk validate -p <name_of_package> --tests --tox-args resilient_password="secret_pwd" resilient_host="ibmsoar.example.com"
+        self.parser.add_argument("-a", "--author",
+                                 type=ensure_unicode,
+                                 required=False,
+                                 help="setup.py author name")
+        self.parser.add_argument("-ae", "--author_email",
+                                 type=ensure_unicode,
+                                 required=False,
+                                 help="setup.py author email")
+        self.parser.add_argument("-u", "--url",
+                                 type=ensure_unicode,
+                                 required=False,
+                                 help="setup.py company URL")
+        self.parser.add_argument("-l", "--license",
+                                type=ensure_unicode,
+                                required=False,
+                                help="setup.py license name")
+
+        # Create an argument for internal use to pre-populate values
+        self.parser.add_argument("-i", "--internal",
+                                required=False,
+                                action='store_true',
+                                help=SUPPRESS)    # Use SUPPRESS to avoid printing this arg when doing `init -h`
+
     def execute_command(self, args):
+
+        # TODO: Validate the number of args
+
         LOG.debug(f"called: CmdRunInit.execute_command()")
         
         # If filename is provided in args, use that, otherwise use default .sdk_settings.json
@@ -64,7 +94,6 @@ class CmdRunInit(BaseCmd):
             overwrite = input(f"{settings_file} exists already. Would you like to overwrite (y/n)? ")
         
         if overwrite.lower() == "n":
-            # TODO: Why does this get printed to console?
             LOG.info(f"Will not overwrite {settings_file}... Exiting CmdRunInit.exeucte_command().")
             return
         
@@ -74,11 +103,18 @@ class CmdRunInit(BaseCmd):
         # Load the Jinja2 Template
         settings_template = jinja_env.get_template(constants.SETTINGS_TEMPLATE_NAME)
 
-        rendered_settings = settings_template.render()
+        LOG.debug("Rendering settings file with provided args")
+        rendered_settings = settings_template.render({
+            "author":  "IBM SOAR" if args.internal else (args.author or "<<your name here>>"),
+            "author_email": "" if args.internal else (args.author_email or "you@example.com"),
+            "url": "https://ibm.com/mysupport" if args.internal else (args.url or "<<your company url>>"),
+            "license": "MIT" if args.internal else (args.license or "<<insert here>>"),
+            "supported_app": "true" if args.internal else "false"
+        })
 
         LOG.info(f"Writing settings to: {settings_file}")
 
-        # Write the new README
+        # Write the new settings.json
         sdk_helpers.write_file(settings_file, rendered_settings)
 
         return
