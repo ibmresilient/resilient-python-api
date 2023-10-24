37e906c6bdd5a6d9247a331b816e1083bd85b26b
Bo Bleckel <bo.bleckel@ibm.com>
2023-10-24 11:47:05 -0400

    SOARAPPS-7319 {resilient-circuits} python 3 only

--- resilient-circuits/tests/test_app_redacting_filter.py
+++ resilient-circuits/tests/test_app_redacting_filter.py
@@ -4,10 +4,14 @@
 
 
 import logging
+import sys
 
-from resilient_circuits.app import RedactingFilter
+import pytest
 from resilient_circuits import constants
+from resilient_circuits.app import RedactingFilter
 
+
+@pytest.mark.skipif(sys.version_info.major < 3, reason="requires python 3")
 def test_clears_sensitive_info(caplog):
     caplog.set_level(logging.DEBUG)
     logger = logging.getLogger("test1")
@@ -23,6 +27,7 @@ def test_clears_sensitive_info(caplog):
     assert "'seems_good': 'found', 'secret': '***'" in caplog.text
     assert "will be removed" not in caplog.text
 
+@pytest.mark.skipif(sys.version_info.major < 3, reason="requires python 3")
 def test_clears_sensitive_info_with_multiple(caplog):
     caplog.set_level(logging.DEBUG)
     logger = logging.getLogger("test2")
@@ -31,15 +36,17 @@ def test_clears_sensitive_info_with_multiple(caplog):
     data = {
         "seems_good": "found",
         "secret": "superdupersecret",
-        "api_key": "12345abcde"
+        "api_key": "12345abcde",
+        "something_that_should_show": "found it!"
     }
 
     logger.info(data)
 
-    assert "'seems_good': 'found', 'secret': '***', 'api_key': '***'" in caplog.text
+    assert "'seems_good': 'found', 'secret': '***', 'api_key': '***', 'something_that_should_show': 'found it!'" in caplog.text
     assert data.get("secret") not in caplog.text
     assert data.get("api_key") not in caplog.text
 
+@pytest.mark.skipif(sys.version_info.major < 3, reason="requires python 3")
 def test_all_password_prefixes_removed(caplog):
     caplog.set_level(logging.DEBUG)
     logger = logging.getLogger("test3")
