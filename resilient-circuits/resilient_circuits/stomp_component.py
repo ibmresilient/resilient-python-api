9feb594a1a21a7e247949978399ebe1e55e404c6
Bo Bleckel <bo.bleckel@ibm.com>
2024-04-24 16:33:56 -0400

    SOARAPPS-7927 {resilient-circuits} replace stompest for stomp.py - major clean up - added more logging

--- resilient-circuits/resilient_circuits/stomp_component.py
+++ resilient-circuits/resilient_circuits/stomp_component.py
@@ -4,9 +4,7 @@
 """ Circuits component for handling Stomp Connection """
 
 import logging
-import ssl
 import sys
-import time
 import traceback
 
 import stomp
@@ -14,7 +12,9 @@ from circuits import BaseComponent
 from circuits.core.handlers import handler
 
 from resilient_circuits import constants
-from resilient_circuits.stomp_events import *
+from resilient_circuits.stomp_events import (Connected, ConnectionFailed,
+                                             Disconnect, Disconnected, Message,
+                                             OnStompError)
 
 DEFAULT_MAX_RECONNECT_ATTEMPTS = 3
 DEFAULT_STARTUP_MAX_RECONNECT_ATTEMPTS = 3
@@ -22,52 +22,77 @@ DEFAULT_STARTUP_MAX_RECONNECT_ATTEMPTS = 3
 LOG = logging.getLogger(__name__)
 
 
-
 class StompClient(BaseComponent):
+    """
+    STOMP Component. Implements the circuits base component and is registered to the component
+    tree to handle events, mostly those events fired from Action component.
+
+    Uses the stomp.py library to implement STOMP connections. The SOARStompListener implements
+    the events triggered by the stomp.py library to handle stomp events from the server.
+    """
 
     channel = "stomp"
 
+    def __init__(self, *args, **kwargs):
+        # define all class variables with initials here, to be actually filled in the 'init' method
+        self._subscribed = {}
+        self._stomp_connection_errors = 0
+        self._stomp_max_connection_errors = None
+        self._api_key = None
+        self._api_secret = None
+        self._stomp_host = tuple()
+        self._stomp_client = None
+
+        # call to super.__init__ must happen at the end so that 'init' method is called
+        # from BaseComponent constructor
+        super(StompClient, self).__init__(*args, **kwargs)
+
     def init(self, host, port,
+             *_args,
              username=None, password=None,
              heartbeats=(0, 0),
              ca_certs=None,
-             stomp_params=None,
-             stomp_max_connection_errors=constants.STOMP_MAX_CONNECTION_ERRORS, *_args, **_kwargs):
+             connect_timeout=120,
+             stomp_max_connection_errors=constants.STOMP_MAX_CONNECTION_ERRORS,
+             heart_beat_receive_scale=2,
+             **_kwargs):
 
         # TODO! Figure out if stomp-py can work with a proxy through a similar mechanism that we're currently using with the stomp_transport.py file
-        LOG.info("Connect to stomp at '%s:%s'", host, port)
+        # TODO: we removed the 'stomp_params' app.config option. do we need to bring back?
+
+        # connect timeout has to be greater than the heartbeat scale
+        # multiplied by the heartbeat for server. otherwise we would get
+        # continuous timeouts on connect (see https://github.com/jasonrbriggs/stomp.py/issues/366)
+        heart_beat_true_seconds = heart_beat_receive_scale * heartbeats[1]/1000 # divide by 1000 to get seconds
+        if heart_beat_true_seconds > connect_timeout:
+            LOG.warning("'stomp_timeout' set to a value lower than heartbeats")
+            connect_timeout = 1.5 * heart_beat_true_seconds # 1.5 scale here to be sure in the clear
+            LOG.info("Automatically adjusting STOMP timeout to '%s' so that it fits outside of heartbeats", connect_timeout)
+
+        self._subscribed = {}
+
+        self._stomp_connection_errors = 0
+        self._stomp_max_connection_errors = stomp_max_connection_errors
+
+        self._api_key = username
+        self._api_secret = password
+        self._stomp_host = [(host, port)]
 
 
-        # Configure failover options so it only tries based on settings
-        # build any parameters passed
-        # every connection has at least these two: maxReconnectAttempts, startupMaxReconnectAttempts
-        items = [item.split("=", 2) for item in stomp_params.split(",")] if stomp_params else []
-        connection_params = {item[0].strip():item[1].strip() for item in items} if items else {}
-
-        if "maxReconnectAttempts" not in connection_params:
-            connection_params["maxReconnectAttempts"] = DEFAULT_MAX_RECONNECT_ATTEMPTS
-        if "startupMaxReconnectAttempts" not in connection_params:
-            connection_params["startupMaxReconnectAttempts"] = DEFAULT_STARTUP_MAX_RECONNECT_ATTEMPTS
-
-        self.api_key = username
-        self.api_secret = password
-        self.stomp_host = [(host, port)]
+        LOG.info("Connect to STOMP at '%s:%s'", host, port)
         self._stomp_client = stomp.StompConnection12(
-            self.stomp_host,
+            self._stomp_host,
             reconnect_attempts_max=-1,
             reconnect_sleep_initial=1,
             reconnect_sleep_increase=3,
+            timeout=connect_timeout,
             keepalive=True,
             heartbeats=heartbeats,
             heart_beat_receive_scale=2
         )
-        self._stomp_client.set_ssl(for_hosts=self.stomp_host, ca_certs=ca_certs)
+        LOG.debug("Set SSL for STOMP client for hosts '%s' and certs '%s'", self._stomp_host, ca_certs)
+        self._stomp_client.set_ssl(for_hosts=self._stomp_host, ca_certs=ca_certs)
         self._stomp_client.set_listener("", SOARStompListener(self))
-
-        self.subscribe_list = []
-
-        self.stomp_connection_errors = 0
-        self.stomp_max_connection_errors = stomp_max_connection_errors
 
 
     @property
@@ -80,32 +105,34 @@ class StompClient(BaseComponent):
 
     @property
     def subscribed(self):
-        return self.subscribe_list
-
-    @property
-    def stomp_logger(self):
-        return __name__
+        return self._subscribed
 
 
     @handler("Connect")
     def connect(self, event, host=None, _subscribe=None):
         """ connect to Stomp server """
-        LOG.info("Connect to Stomp...")
+        LOG.info("Connect to STOMP...")
         try:
-            self._stomp_client.connect(username=self.api_key, passcode=self.api_secret, wait=True, with_connect_command=True)
+            self._stomp_client.connect(
+                username=self._api_key,
+                passcode=self._api_secret,
+                wait=True,
+                with_connect_command=True
+            )
             # LOG.debug("State after Connection Attempt: %s", self._client.session.state)
             if self.connected:
-                LOG.info("Connected to STOMP at %s", self.stomp_host)
+                LOG.info("Connected to STOMP at %s", self._stomp_host)
                 self.fire(Connected())
-                self.stomp_connection_errors = 0 # restart counter
+                self._stomp_connection_errors = 0 # restart counter
                 return "success"
 
         except (stomp.exception.ConnectFailedException, stomp.exception.NotConnectedException) as err:
             LOG.debug(traceback.format_exc())
-            # is this error unrecoverable?
+            # Since switching to stomp.py library, this error hasn't been seen
+            # but we're keeping around just in case it pops up somewhere...
             if "no more data" in str(err).lower():
-                self.stomp_connection_errors += 1
-                if self.stomp_max_connection_errors and self.stomp_connection_errors >= self.stomp_max_connection_errors:
+                self._stomp_connection_errors += 1
+                if self._stomp_max_connection_errors and self._stomp_connection_errors >= self._stomp_max_connection_errors:
                     LOG.error("Exiting due to unrecoverable error")
                     sys.exit(1) # this will exit resilient-circuits
 
@@ -117,13 +144,16 @@ class StompClient(BaseComponent):
             LOG.error(traceback.format_exc())
             LOG.error("Exiting due to unrecoverable error")
             sys.exit(1) # this will exit resilient-circuits
-        # return "fail"
+
         event.success = False
-        self.fire(ConnectionFailed(self.stomp_host))
+        self.fire(ConnectionFailed(self._stomp_host))
+        return "fail"
 
     @handler("Disconnect")
     def _disconnect(self, receipt=None, flush=True, reconnect=False):
         try:
+            if flush:
+                self._subscribed = {}
             if self.connected:
                 self._stomp_client.disconnect(receipt=receipt)
         except stomp.exception.StompException:
@@ -135,12 +165,12 @@ class StompClient(BaseComponent):
 
     @handler("Unsubscribe")
     def _unsubscribe(self, event, destination):
-        # if destination not in self._subscribed:
-        #     LOG.error("Unsubscribe Request Ignored. Not subscribed to %s", destination)
-        #     return
+        if destination not in self._subscribed:
+            LOG.error("Unsubscribe request ignored. Not subscribed to '%s'", destination)
+            return
         try:
-            # token = self._subscribed.pop(destination)
             self._stomp_client.unsubscribe(destination)
+            self._subscribed.pop(destination)
             LOG.debug("Unsubscribed: %s", destination)
         except stomp.exception.StompException:
             event.success = False
@@ -149,8 +179,8 @@ class StompClient(BaseComponent):
 
     @handler("Subscribe")
     def _subscribe(self, event, destination, additional_headers=None):
-        # if destination in self._stomp_client.:
-        #     LOG.debug("Ignoring subscribe request to %s. Already subscribed.", destination)
+        if destination in self._subscribed:
+            LOG.debug("Ignoring subscribe request to %s. Already subscribed.", destination)
         try:
             headers = {"ack": "client-individual",
                        "id": destination}
@@ -159,8 +189,7 @@ class StompClient(BaseComponent):
 
             # Set ID to match destination name for easy reference later
             self._stomp_client.subscribe(destination=destination, id=destination, ack="client-individual", headers=headers)
-            self.subscribe_list.append(destination)
-            # self._subscribed[destination] = True
+            self._subscribed[destination] = True
         except stomp.exception.StompException as err:
             LOG.error("Failed to subscribe to queue.")
             event.success = False
@@ -205,13 +234,15 @@ class StompClient(BaseComponent):
     def on_message(self, *_args, **_kwargs):
         # the rest of this logic is handled in actions_component.on_stomp_message
         # which handles the same "Message" event
-        LOG.debug("Stomp message received")
+        LOG.debug("STOMP message received")
 
 class SOARStompListener(stomp.ConnectionListener):
 
     def __init__(self, component):
         super(SOARStompListener, self).__init__()
         self.component = component
+
+        LOG.debug("STOMP listener initiated and listening for STOMP events")
 
     def on_error(self, frame):
         LOG.info("Received an error '%s'", frame.headers.get("message", "UNKNOWN"))
@@ -222,10 +253,10 @@ class SOARStompListener(stomp.ConnectionListener):
             # because it just means that ActiveMQ is up, but not authenticatable;
             # stomp client will automatically disconnect and reconnect once the authentication
             # service is back up
-            LOG.info("Messaging service is up but authentication service is not. Disconnect to retry...")
+            LOG.warning("Messaging service is up but authentication service is not. Disconnect to retry...")
 
     def on_message(self, frame):
-        LOG.info("Received message from '%s'", frame.headers.get("reply-to"))
+        LOG.debug("STOMP received message from destination: '%s'", frame.headers.get("reply-to"))
         self.component.fire(Message(frame))
 
     def on_disconnected(self):
@@ -233,10 +264,13 @@ class SOARStompListener(stomp.ConnectionListener):
         self.component.fire(Disconnect(reconnect=True))
 
     def on_connected(self, frame):
-        LOG.info("Connected!")
+        LOG.debug("STOMP connected!")
 
     def on_connecting(self, host_and_port):
-        LOG.info("Connecting to '%s'", host_and_port)
+        LOG.debug("STOMP connecting to '%s'", host_and_port)
+
+    def on_heartbeat(self):
+        LOG.debug("STOMP heartbeat received")
 
     def on_heartbeat_timeout(self):
-        LOG.error("Heartbeat timed-out...")
+        LOG.error("STOMP heartbeat timed-out...")
--- resilient-circuits/tests/test_stomp_component.py
+++ resilient-circuits/tests/test_stomp_component.py
@@ -15,7 +15,7 @@ def test_stomp_reconnect_two_consecutive_failures(caplog):
 
     client = StompClient(host="example.com", port=443)
 
-    assert client.stomp_max_connection_errors == constants.STOMP_MAX_CONNECTION_ERRORS
+    assert client._stomp_max_connection_errors == constants.STOMP_MAX_CONNECTION_ERRORS
 
     # standard successfull connection
     mock_evt = MagicMock()
@@ -29,7 +29,7 @@ def test_stomp_reconnect_two_consecutive_failures(caplog):
             # first a StompConnectionError with a different error (this could be possible in real world)
             patch_stomp_library_connect.side_effect = ConnectFailedException("A different STOMP error")
             client.connect(mock_evt)
-            assert client.stomp_connection_errors == 0
+            assert client._stomp_connection_errors == 0
 
             # now a StompConnectionError with "no more data" error
             # this should trigger the max, and throw a sys.exit(1)
@@ -37,14 +37,14 @@ def test_stomp_reconnect_two_consecutive_failures(caplog):
             with pytest.raises(SystemExit):
                 client.connect(mock_evt)
                 assert "Exiting due to unrecoverable error" in caplog.text
-            assert client.stomp_connection_errors == 1
+            assert client._stomp_connection_errors == 1
 
 def test_stomp_reconnect_one_failure_followed_by_successful_reconnect(caplog):
 
     max_connection_errors_override = 5
     client = StompClient(host="example.com", port=443, stomp_max_connection_errors=max_connection_errors_override)
 
-    assert client.stomp_max_connection_errors == max_connection_errors_override
+    assert client._stomp_max_connection_errors == max_connection_errors_override
 
     # standard successfull connection
     mock_evt = MagicMock()
@@ -60,12 +60,12 @@ def test_stomp_reconnect_one_failure_followed_by_successful_reconnect(caplog):
                 # first StompConnectionError with "no more data" error
                 patch_stomp_library_connect.side_effect = ConnectFailedException("No more data received from STOMP")
                 client.connect(mock_evt)
-                assert client.stomp_connection_errors == i+1
+                assert client._stomp_connection_errors == i+1
 
             # successful reconnect now should reset count to 0
             patch_stomp_library_connect.side_effect = None
             client.connect(mock_evt)
-            assert client.stomp_connection_errors == 0
+            assert client._stomp_connection_errors == 0
 
 # def test_stomp_client_logging_check_server_heartbeat(caplog):
 #     caplog.set_level(logging.DEBUG)
