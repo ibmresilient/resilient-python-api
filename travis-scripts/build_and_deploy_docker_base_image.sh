ea367c82596b1b91e02bdf0eb5d5e86a94b46b82
Bo Bleckel <bo.bleckel@ibm.com>
2024-08-26 11:14:56 -0400

    SOARAPPS-8259 start build Python 3.12 base image - SOARAPPS-8261 rebuild Quay images on master CRON build - also stop building 3.9 base image

--- travis-scripts/build_and_deploy_docker_base_image.sh
+++ travis-scripts/build_and_deploy_docker_base_image.sh
@@ -12,8 +12,8 @@
 #  - v51.0.0.0.1234
 #  - python-311
 #  - v51.0.0.0.1234-python-311
-#  - python-39
-#  - v51.0.0.0.1234-python-39
+#  - python-312
+#  - v51.0.0.0.1234-python-312
 
 DOCKER_REPO_PATH=$1 # ex: docker-eu.artifactory.swg-devops.com/sec-resilient-docker-local OR quay.io
 CIRCUITS_VERSION=$2
@@ -37,10 +37,10 @@ CIRCUITS_VERSION:\t\t${CIRCUITS_VERSION}\n\
 tag_base="${DOCKER_REPO_PATH}/ibmresilient/${DOCKER_IMAGE_NAME}"
 tag_version="${tag_base}:${CIRCUITS_VERSION}"
 tag_latest="${tag_base}:latest"
-tag_version_39="${tag_base}:${CIRCUITS_VERSION}-python-39"
-tag_python_39="${tag_base}:python-39"
 tag_version_311="${tag_base}:${CIRCUITS_VERSION}-python-311"
 tag_python_311="${tag_base}:python-311"
+tag_version_312="${tag_base}:${CIRCUITS_VERSION}-python-312"
+tag_python_312="${tag_base}:python-312"
 
 # build PY311 and tag appropriately with "<version>", "<version>-python-311", "python-311", "latest"
 docker build -t ${tag_version} -t ${tag_version_311} -t ${tag_python_311} -t ${tag_latest} \
@@ -48,11 +48,19 @@ docker build -t ${tag_version} -t ${tag_version_311} -t ${tag_python_311} -t ${t
     --build-arg PYTHON_VERSION=python-311 \
     ${TRAVIS_BUILD_DIR}
 
-# build PY39 version and tag with "<version>-python-39", "python-39"
-docker build -t ${tag_version_39} -t ${tag_python_39} \
+# build PY312 version and tag with "<version>-python-312", "python-312"
+docker build -t ${tag_version_312} -t ${tag_python_312} \
     --build-arg RESILIENT_CIRCUITS_VERSION=${CIRCUITS_VERSION} \
-    --build-arg PYTHON_VERSION=python-39 \
+    --build-arg PYTHON_VERSION=python-312 \
     ${TRAVIS_BUILD_DIR}
 
-# push all versions to their tagged location
-docker push ${tag_base} --all-tags
+
+if [ $TRAVIS_EVENT_TYPE == "cron" ]; then
+    # when running on a CRON build, only push the latest, and two Python versions
+    docker push ${tag_latest}
+    docker push ${tag_python_311}
+    docker push ${tag_python_312}
+else
+    # push all versions to their tagged location
+    docker push ${tag_base} --all-tags
+fi
