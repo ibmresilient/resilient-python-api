b4f31cb807bca9285ac4d52292798d5d074ecd35
Priya-Sapra <priya.sapra@ibm.com>
2024-10-11 11:58:15 -0400

    SOARAPPS-7837 {resilient-circuits} test low code message

--- resilient-circuits/tests/test_actions_component.py
+++ resilient-circuits/tests/test_actions_component.py
@@ -7,7 +7,7 @@ from mock import patch
 from resilient_circuits import helpers, SubmitTestFunction
 from resilient_circuits.actions_component import Actions
 from resilient_circuits.stomp_events import HeartbeatTimeout
-from resilient_circuits.constants import SUBSCRIBE_DTO
+from resilient_circuits.constants import LOW_CODE_MSG_DEST_PREFIX, SUBSCRIBE_DTO, REST_REQUEST_DTO
 from resilient_lib import IntegrationError
 
 from tests import MockInboundAppComponent
@@ -16,6 +16,7 @@ from tests import mock_constants
 from tests.shared_mock_data import mock_paths
 from circuits import Event
 from stomp.utils import Frame
+import json
 
 
 resilient_mock = mock_constants.RESILIENT_MOCK
@@ -73,18 +74,24 @@ def test_actions_on_heartbeat_timeout_with_config_set(circuits_app, fx_simple_cl
         assert sys_exit.type == SystemExit
         assert sys_exit.value.code == 34
 
+@pytest.mark.parametrize("message_headers, message, expected_log", [
+    ({"Co3MessagePayload": SUBSCRIBE_DTO}, '{"subscribe":["test_queue"]}', "new connector queue"),       # Tests new connector message
+    ({"Co3MessagePayload": REST_REQUEST_DTO}, '{"my":"test message"}', f"Channel: {LOW_CODE_MSG_DEST_PREFIX}")
+])
 @patch("resilient_circuits.actions_component.helpers.get_fn_names", new=lambda x: [])
-def test_on_stomp_message(circuits_app, fx_simple_client, caplog):
+def test_on_stomp_message(circuits_app, fx_simple_client, caplog, message_headers, message, expected_log):
+    # mock and patch as necessary
     mock_configs = helpers.get_configs(path_config_file=mock_paths.MOCK_APP_CONFIG)
     mock_configs.pop("heartbeat_timeout_threshold")
     with patch("resilient_circuits.actions_component.ResilientComponent.rest_client") as mock_client:
         mock_client.return_value = fx_simple_client[0]
         mock_actions_cmp = Actions(opts=mock_configs)
+
+        # set up stomp frame and message event
         frame_headers = {"message-id": 1}
-        message = "this is a message"
-        frame = Frame(cmd="MESSAGE", headers=frame_headers, body='{"subscribe":["test_queue"]}')
-        message_headers = {"Co3MessagePayload": SUBSCRIBE_DTO}
+        frame = Frame(cmd="MESSAGE", headers=frame_headers, body=message)
         evt = Event("Message")
         evt.frame = frame
-        mock_actions_cmp.on_stomp_message(evt, message_headers, '{"subscribe":["test_queue"]}', "actions.202.main_queue")
-        assert "new connector queue" in caplog.text
+
+        mock_actions_cmp.on_stomp_message(evt, message_headers, message, "actions.202.main_queue")
+        assert expected_log in caplog.text
