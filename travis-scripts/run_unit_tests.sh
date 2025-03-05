5b5226639aab00bd83a792fffe16afef396697dc
Mark Scherfling <mscherfling@gmail.com>
2025-03-05 13:47:04 -0500

    Delete travis-scripts directory

--- travis-scripts/run_unit_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-tox -c ./resilient -- -m "not livetest"
-tox -c ./resilient-circuits -- -m "not livetest"
-tox -c ./resilient-sdk -e UNIT -- -m "not livetest"
-tox -c ./resilient-lib -- -m "not livetest"
